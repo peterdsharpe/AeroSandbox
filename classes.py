1de494a4ee5a18978b4fd306cce4bc8fff24e386
peterdsharpe <peterdsharpe@gmail.com>
2019-05-16 00:37:40 -0400

    fixing the class struggle

--- classes.py
+++ classes.py
@@ -10,3 +10,36 @@ class Airplane:
         self.name = name
         self.XYZle = np.array(XYZle)
         self.wings = wings
+
+
+class Wing:
+    def __init__(self,
+                 name="Untitled",
+                 XYZle=[0, 0, 0],
+                 sections=[]
+                 ):
+        self.name = name
+        self.XYZle = np.array(XYZle)
+        self.sections = sections
+
+
+class Wingsection:
+    def __init__(self,
+                 XYZle=[0, 0, 0],
+                 chord=0,
+                 twist=0,
+                 airfoil=[]
+                 ):
+        self.XYZle = np.array(XYZle)
+        self.chord = chord
+        self.twist = twist
+        self.airfoil = airfoil
+
+
+class Airfoil:
+    def __init__(self,
+                 name="naca0012",
+                 REref=1e6
+                 ):
+        self.name=name
+        self.REref=REref
--- makeConventional.py
+++ makeConventional.py
@@ -5,16 +5,27 @@ p = Airplane(
     XYZle=[1, 0, 0],
     wings=[
         Wing(
-            name="Main Wing"
+            name="Main Wing",
             XYZle=[0, 0, 0],
             sections=[
-                Wingsection( # Root
+                Wingsection(  # Root
                     XYZle=[0, 0, 0],
                     chord=0.2,
                     twist=0,
-                    airfoil=naca4412
+                    airfoil=Airfoil("naca4412")
+                ),
+                Wingsection(  # Mid
+                    XYZle=[0.025, 0.4, 0],
+                    chord=0.15,
+                    twist=0,
+                    airfoil=Airfoil("naca4412")
+                ),
+                Wingsection(  # Tip
+                    XYZle=[0.075, 0.6, 0.1],
+                    chord=0.05,
+                    twist=0,
+                    airfoil=Airfoil("naca4412")
                 )
-
             ]
         )
     ],
