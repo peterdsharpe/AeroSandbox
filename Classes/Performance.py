1e0a89d2dfbf58367a12900ebddc7697297b5af7
peterdsharpe <peterdsharpe@gmail.com>
2019-05-23 09:11:13 -0400

    nightly

--- Classes/Performance.py
+++ Classes/Performance.py
@@ -3,6 +3,7 @@ import math
 import matplotlib.pyplot as plt
 from .Plotting import *
 
+
 class OperatingPoint:
     def __init__(self,
                  velocity=10,
@@ -10,4 +11,27 @@ class OperatingPoint:
                  beta=0,
                  p=0,
                  q=0,
-                 r=0
+                 r=0,
+                 ):
+        self.velocity = velocity
+        self.alpha = alpha
+        self.beta = beta
+        self.p = p
+        self.q = q
+        self.r = r
+
+    def compute_freestream_velocity(self):
+        # Computes the freestream velocity vector in aircraft geometry coordinates
+
+        sinalpha = np.sin(np.radians(self.alpha))
+        cosalpha = np.cos(np.radians(self.alpha))
+        sinbeta = np.sin(np.radians(self.beta))
+        cosbeta = np.cos(np.radians(self.beta))
+
+        vel_vec = self.velocity * np.array([
+            cosalpha * cosbeta,
+            -sinbeta,
+            sinalpha * cosbeta,
+        ])
+
+        return vel_vec
--- Classes/__init__.py
+++ Classes/__init__.py
@@ -1,3 +1,4 @@
 from .Aerodynamics import *
 from .Geometry import *
 from .Plotting import *
+from .Performance import *
