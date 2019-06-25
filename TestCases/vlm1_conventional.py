f164fe0e5bcc2c748d217089a58e09ca05d52e5b
peterdsharpe <peterdsharpe@gmail.com>
2019-06-24 20:42:20 -0400

    Update vlm1_conventional.py

--- TestCases/vlm1_conventional.py
+++ TestCases/vlm1_conventional.py
@@ -7,7 +7,7 @@ ap = vlm1(
     airplane=a,
     op_point=OperatingPoint(velocity=10,
                             alpha=5,
-                            beta=5),
+                            beta=0),
 )
 ap.run()
 ap.draw()
