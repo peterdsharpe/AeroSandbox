b01d3ea7007f9fe10c64418b23696408ef2d0458
peterdsharpe <peterdsharpe@gmail.com>
2020-02-15 13:49:21 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.0',  # Required
+    version='1.0.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -141,7 +141,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pyvista', 'autograd', 'numpy', 'numba', 'matplotlib', 'scipy'],
+    install_requires=['numpy', 'casadi', 'plotly'],
     # Optional
 
     # List additional groups of dependencies here (e.g. development
