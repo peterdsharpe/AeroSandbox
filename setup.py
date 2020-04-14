2196c58a01735c6b646d0de169c017bac243ae10
Peter Sharpe <peterdsharpe@gmail.com>
2020-04-13 21:03:50 -0500

    include all data

--- setup.py
+++ setup.py
@@ -165,9 +165,9 @@ setup(
     # If using Python 2.6 or earlier, then these have to be included in
     # MANIFEST.in as well.
     include_package_data=True,
-    # package_data={  # Optional
-    #     'sample': ['package_data.dat'],
-    # },
+    package_data={  # Optional
+        '': ['*'], # include all data
+    },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
