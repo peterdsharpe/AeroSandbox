10647623e150f6b96b56e304cf91f540b1e71ec4
Peter Sharpe <peterdsharpe@gmail.com>
2020-04-19 15:51:14 -0500

    fitter progress

--- aerosandbox/tools/airfoil_fitter/airfoil_fitter.py
+++ aerosandbox/tools/airfoil_fitter/airfoil_fitter.py
@@ -16,7 +16,7 @@ class AirfoilFitter():
         self.airfoil = airfoil
 
     def get_xfoil_data(self,
-                       a_start=-5,
+                       a_start=-6,
                        a_end=12,
                        a_step=0.5,
                        a_init=0,
@@ -209,7 +209,8 @@ try:
     with open("af.pkl", "rb") as f:
         af = pickle.load(f)
 except:
-    af = AirfoilFitter(Airfoil(name="HALE_thiqboi_02", coordinates="C:/Users/User/Downloads/HALE_02.dat"))
+    # af = AirfoilFitter(Airfoil(name="HALE_thiqboi_02", coordinates="C:/Users/User/Downloads/HALE_02.dat"))
+    af = AirfoilFitter(Airfoil(name="naca0010"))
     af.get_xfoil_data(
         a_step=0.5,
         n_Res=30,
@@ -221,8 +222,8 @@ except:
 af.plot_xfoil_data()
 self = af
 
-supercritical_threshold = 5e5
-subcritical_threshold = 3e4
+supercritical_threshold = 1e6
+subcritical_threshold = 1e4
 
 ### Fit utilities, data extraction, plotting tools
 d = self.xfoil_data_1D  # data
@@ -263,8 +264,8 @@ def plot_fit_alpha_Re(
             go.Mesh3d(
                 x=x_data['alpha'],
                 y=x_data['Re'],
-                z=model(d, params_solved),
-                intensity=model(d, params_solved),
+                z=np.array(model(d, params_solved)).reshape(-1),
+                intensity=np.array(model(d, params_solved)).reshape(-1),
                 colorscale="plasma",
                 flatshading=True
             ),
@@ -299,15 +300,16 @@ def model_Cl_turbulent(x, p):
 
 
 Cl_turbulent_params_guess = {
-    'clt_a_c' : 3,
-    'clt_a_s' : 8,
-    'clt_cl_c': 0.4,
-    'clt_cl_s': 1,
+    'clt_a_c' : 0,
+    'clt_a_s' : 12,
+    'clt_cl_c': 0,
+    'clt_cl_s': 1.5,
 }
 Cl_turbulent_param_bounds = {
-    'clt_a_s' : (0, 15),
-    'clt_cl_c': (0, 1.5),
-    'clt_cl_s': (0, 2),
+    'clt_a_c' : (0, None),
+    'clt_a_s' : (0, 20),
+    'clt_cl_c': (0, None),
+    'clt_cl_s': (0, 2.5),
 }
 
 Cl_turbulent_params_solved = fit(
@@ -316,41 +318,46 @@ Cl_turbulent_params_solved = fit(
     y_data=d['Cl'],
     param_guesses=Cl_turbulent_params_guess,
     param_bounds=Cl_turbulent_param_bounds,
-    weights=(d['Re'] > supercritical_threshold).astype('int')
+    weights=np.logical_and(d['Re'] >= supercritical_threshold, True).astype('int')
 )
 
-plot_fit_alpha_Re(
-    model=model_Cl_turbulent,
-    x_data=d,
-    y_data=d['Cl'],
-    params_solved=Cl_turbulent_params_solved,
-    title="Fit: Lift Coefficient (Turbulent)"
-)
+# plot_fit_alpha_Re(
+#     model=model_Cl_turbulent,
+#     x_data=d,
+#     y_data=d['Cl'],
+#     params_solved=Cl_turbulent_params_solved,
+#     title="Fit: Lift Coefficient (Turbulent)"
+# )
 
 
 ### Fit the subcritical data
 def model_Cl_laminar(x, p):
     Cl_laminar = (
             sigmoid(x['alpha'], p['cll_a_c'], p['cll_a_s'], p['cll_cl_c'], p['cll_cl_s']) +
-            sigmoid(x['alpha'], p['cll_a_c'], p['cll_a_s'], p['cll_cl_c'], p['cll_cl_s'])
+            sigmoid(x['alpha'], p['clld_a_c'], p['clld_a_s'], p['clld_cl_c'], p['clld_cl_s'])
     )
     return Cl_laminar
 
 
 Cl_laminar_params_guess = {
-    'cll_a_c'  : 3,
-    'cll_a_s'  : 8,
-    'cll_cl_c' : 0.4,
-    'cll_cl_s' : 1,
-    'clld_a_c' : -1,
-    'clld_a_s' : 2,
-    'clld_cl_c': 0.1,
+    'cll_a_c'  : 0,
+    'cll_a_s'  : 12,
+    'cll_cl_c' : 0,
+    'cll_cl_s' : 1.5,
+    'clld_a_c' : 0,
+    'clld_a_s' : 1,
+    'clld_cl_c': 0,
     'clld_cl_s': 0.1,
 }
 Cl_laminar_param_bounds = {
-    'cll_a_s' : (0, 15),
-    'cll_cl_c': (0, 1.5),
-    'cll_cl_s': (0, 2),
+    'cll_a_c'  : (0, None),
+    'cll_a_s'  : (0, 20),
+    'cll_cl_c' : (0, None),
+    'cll_cl_s' : (0, 2.5),
+    'clld_a_c' : (-8, 8),
+    'clld_a_s' : (0, 8),
+    'clld_cl_c': (0, None),
+    'clld_cl_s': (0, 0.4),
 }
 
 Cl_laminar_params_solved = fit(
@@ -359,7 +366,7 @@ Cl_laminar_params_solved = fit(
     y_data=d['Cl'],
     param_guesses=Cl_laminar_params_guess,
     param_bounds=Cl_laminar_param_bounds,
-    weights=(d['Re'] < subcritical_threshold).astype('int')
+    weights=np.logical_and(d['Re'] <= subcritical_threshold, True).astype('int')
 )
 
 plot_fit_alpha_Re(
@@ -367,5 +374,55 @@ plot_fit_alpha_Re(
     x_data=d,
     y_data=d['Cl'],
     params_solved=Cl_laminar_params_solved,
-    title="Fit: Lift Coefficient (Turbulent)"
+    title="Fit: Lift Coefficient (Laminar)"
+)
+
+
+# Fit the blend
+def model_Cl_blend(x, p):
+    log10_Re = cas.log10(x['Re'])
+    blend_input = -p['clb_hardness'] * (
+            p['clb_a_scale'] * (5 + (x['alpha'] - p['clb_a_0']) ** 2) ** 0.5 + p['clb_re_0']
+            - log10_Re
+    )
+    blend = sigmoid(blend_input, 0, 1, 0.5, 0.5)
+    Cl = blend * model_Cl_turbulent(x, p) + (1 - blend) * model_Cl_laminar(x, p)
+    return Cl
+
+
+Cl_blend_params_guess = {
+    **Cl_turbulent_params_solved,
+    **Cl_laminar_params_solved,
+    'clb_hardness' : 1.7,
+    'clb_a_scale'  : 0.3,
+    'clb_a_0'      : 6,
+    'clb_re_0'     : 3.5,
+
+}
+Cl_blend_param_bounds = {
+    # **{k: (v, v) for k, v in Cl_laminar_params_solved.items()},
+    # **{k: (v, v) for k, v in Cl_turbulent_params_solved.items()},
+    **Cl_laminar_param_bounds,
+    **Cl_turbulent_param_bounds,
+    'clb_hardness' : (1e-3, 10),
+    'clb_a_scale'  : (0, 1),
+    'clb_a_0'      : (-4, 12),
+    'clb_re_0'     : (3, 6),
+}
+
+Cl_blend_params_solved = fit(
+    model=model_Cl_blend,
+    x_data=d,
+    y_data=d['Cl'],
+    param_guesses=Cl_blend_params_guess,
+    param_bounds=Cl_blend_param_bounds,
+    # weights=(d['Cl'] >= 0).astype('int')
+)
+
+plot_fit_alpha_Re(
+    model=model_Cl_blend,
+    x_data=d,
+    y_data=d['Cl'],
+    params_solved=Cl_blend_params_solved,
+    title="Fit: Lift Coefficient (Blend)"
 )
