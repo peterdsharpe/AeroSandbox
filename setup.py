19cfc6f7553e5540eb0e5372699b910a0f9e4599
Peter Sharpe <peterdsharpe@gmail.com>
2020-05-01 22:03:49 -0500

    version increment

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="2.1.2",
+    version="2.2.0",
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
