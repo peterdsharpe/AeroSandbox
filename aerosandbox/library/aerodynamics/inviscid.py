f989173c71581e9460cd017ce9b09b075bccaaa0
Peter Sharpe <peterdsharpe@gmail.com>
2020-11-24 15:23:33 -0500

    Needed to add viscous.py to git

--- aerosandbox/library/aerodynamics/inviscid.py
+++ aerosandbox/library/aerodynamics/inviscid.py
@@ -3,6 +3,95 @@ import numpy as np
 from aerosandbox.tools.casadi_tools import *
 
 
+def induced_drag(
+        lift,
+        span,
+        dynamic_pressure,
+        oswalds_efficiency=1,
+):
+    """
+    Computes the induced drag associated with a lifting planar wing.
+
+    Args:
+        lift: Lift force [Newtons]
+        span: Wing span [meters]
+        dynamic_pressure: Dynamic pressure [Pascals]
+        oswalds_efficiency: Oswald's efficiency factor [-]
+
+    Returns: Induced drag force [Newtons]
+
+    """
+    return lift ** 2 / (
+            dynamic_pressure * np.pi * span ** 2 * oswalds_efficiency
+    )
+
+
+def oswalds_efficiency(
+        taper_ratio,
+        AR,
+        sweep=0,
+        fuselage_diameter = 0,
+):
+    """
+    Computes the Oswald's efficiency factor for a planar, tapered, swept wing.
+
+    Based on "Estimating the Oswald Factor from Basic Aircraft Geometrical Parameters"
+    by M. Nita, D. Scholz; Hamburg Univ. of Applied Sciences, 2012.
+
+    Implementation of Section 5 from the above paper.
+
+    Only valid for backwards-swept wings; i.e. 0 <= sweep < 90.
+
+    Args:
+        taper_ratio: Taper ratio of the wing (tip_chord / root_chord) [-]
+        AR: Aspect ratio of the wing (b^2 / S) [-]
+        sweep: Wing quarter-chord sweep angle [deg]
+
+    Returns: Oswald's efficiency factor [-]
+
+    """
+
+    def f(l):  # f(lambda), given as Eq. 36 in the Nita and Scholz paper (see parent docstring).
+        return (
+                0.0524 * l ** 4
+                - 0.15 * l ** 3
+                + 0.1659 * l ** 2
+                - 0.0706 * l
+                + 0.0119
+        )
+
+    delta_lambda = -0.357 + 0.45 * cas.exp(-0.0375 * sweep)
+    # Eq. 37 in Nita & Scholz.
+    # Note: there is a typo in the cited paper; the negative in the exponent was omitted.
+    # A bit of thinking about this reveals that this omission must be erroneous.
+
+    e_theo = 1 / (
+            1 + f(taper_ratio - delta_lambda) * AR
+    )
+
+    return e_theo
+
+
+def optimal_taper_ratio(
+        sweep=0,
+):
+    """
+    Computes the optimal (minimum-induced-drag) taper ratio for a given quarter-chord sweep angle.
+
+    Based on "Estimating the Oswald Factor from Basic Aircraft Geometrical Parameters"
+    by M. Nita, D. Scholz; Hamburg Univ. of Applied Sciences, 2012.
+
+    Only valid for backwards-swept wings; i.e. 0 <= sweep < 90.
+
+    Args:
+        sweep: Wing quarter-chord sweep angle [deg]
+
+    Returns: Optimal taper ratio
+
+    """
+    return 0.45 * cas.exp(-0.0375 * sweep)
+
+
 def CL_over_Cl(AR, mach=0, sweep=0):
     """
     Returns the ratio of 3D lift coefficient (with compressibility) to 2D lift coefficient (incompressible).
---
+++ studies/OswaldsEfficiencyTaperRatio/README.md
@@ -0,0 +1,3 @@
+# Oswald's Efficiency
+
+A fit to determine the Oswald's efficiency of a tapered, untwisted wing.
