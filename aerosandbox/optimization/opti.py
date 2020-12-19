e47c46f0d54cf9a3ce98bab50c20a7d63a074fdd
Peter Sharpe <peterdsharpe@gmail.com>
2020-12-19 15:04:19 -0500

    Changes to opti.py: added parameter rewrite. More documentation. Added functionality to track variables, constraints, and parameters from Opti class using pointers. Changed subject_to() to return the dual variable associated with it.

--- aerosandbox/optimization/opti.py
+++ aerosandbox/optimization/opti.py
@@ -8,13 +8,25 @@ class Opti(cas.Opti):
     def __init__(self,
                  variable_categories_to_freeze=[],
                  ):
+
+        # Parent class initialization
         super().__init__()
+
+        # Set default solver settings.
         p_opts = {}
         s_opts = {}
         s_opts["max_iter"] = 3000
         s_opts["mu_strategy"] = "adaptive"
         self.solver('ipopt', p_opts, s_opts)  # Default to IPOPT solver
-        self.categories_to_freeze = variable_categories_to_freeze
+
+        # Start tracking variables, constraints (dual variables), and parameters for operations later on.
+        self.variable_list = []
+        self.constraints_list = []
+        self.parameter_list = []
+
+        # Start tracking variable categories
+        self.variable_categories = []
+        self.variable_categories_to_freeze = variable_categories_to_freeze
 
     def variable(self,
                  n_vars: int = 1,
@@ -31,9 +43,9 @@ class Opti(cas.Opti):
         although these are not strictly required.
 
         Args:
-            n_vars: Number of variables to initialize (used to initialize a vector of variables). If you are
+            n_vars: [Optional] Number of variables to initialize (used to initialize a vector of variables). If you are
                 initializing a scalar variable (the most typical case), leave this equal to 1. When using vector variables,
-                individual components of this vector of variables can be accessed via indexing.
+                individual components of this vector of variables can be accessed via normal indexing.
 
                 Example:
                     >>> opti = asb.Opti()
@@ -41,7 +53,7 @@ class Opti(cas.Opti):
                     >>> opti.subject_to(my_var[3] >= my_var[2])  # This is a valid way of indexing
                     >>> my_sum = asb.cas.sum1(my_var)  # This will sum up all elements of `my_var`
 
-            init_guess: Initial guess for the variable being initialized. For scalar variables, this should be a
+            init_guess: [Optional] Initial guess for the variable being initialized. For scalar variables, this should be a
                 float. For vector variables (see `n_vars`), you can provide either a float (in which case all elements
                 of the vector will be initialized to the given value) or an iterable of equal length (in which case
                 each element will be initialized to the corresponding value in the given iterable).
@@ -53,10 +65,14 @@ class Opti(cas.Opti):
                 If not specified, initial guess defaults to 0 for non-log-transformed variables and 1 for
                 log-transformed variables.
 
-            scale: Approximate scale of the variable 
+            scale: [Optional] Approximate scale of the variable. If not specified, defaults to the supplied initial
+                guess if one exists; otherwise, defaults to 1.
 
-            log_transform:
-            category:
+            log_transform: [Optional] Advanced use only. A flag of whether to internally-log-transform this variable
+            before passing it to the optimizer. Good for known positive engineering quantities that become nonsensical
+            if negative (e.g. mass). Log-transforming these variables can help maintain convexity.
+
+            category: [Optional] What category of variables does this belong to
 
         Returns:
             The variable itself as a symbolic CasADi variable (MX type).
@@ -78,8 +94,12 @@ class Opti(cas.Opti):
         if np.any(scale <= 0):
             raise ValueError("The 'scale' argument must be a positive number.")
 
+        # Add the category name to the list to be tracked
+        if category not in self.variable_categories:
+            self.variable_categories.append(category)
+
         # If the variable is in a category to be frozen, fix the variable at the initial guess.
-        if category in self.categories_to_freeze:
+        if category in self.variable_categories_to_freeze:
             freeze = True
 
         # If the variable is to be frozen, return the initial guess. Otherwise, define the variable using CasADi symbolics.
@@ -95,27 +115,55 @@ class Opti(cas.Opti):
                 var = cas.exp(log_var)
                 self.set_initial(log_var, cas.log(init_guess))
 
+        # Add the variable to the variable list to be tracked
+        self.variable_list.append(var)
+
         return var
 
     def subject_to(self,
                    constraint: Union[cas.MX, bool, List],
-                   ) -> None:
+                   ) -> cas.MX:
+        """
+        Initialize a new constraint(s).
+
+        Args:
+            constraint: A constraint that you want to hold true at the optimum. Example:
+                >>> x = opti.variable()
+                >>> opti.subject_to(x >= 5)
+                You can also pass in a list of multiple constraints using list syntax. For example:
+                >>> x = opti.variable()
+                >>> opti.subject_to([
+                >>>     x >= 5,
+                >>>     x <= 10
+                >>> ])
+
+
+        Returns: The dual variable associated with the new constraint. If the `constraint` input is a list, returns
+            a list of dual variables.
+
+        """
         # Determine whether you're dealing with a single (possibly vectorized) constraint or a list of constraints.
         # If the latter, recursively apply them.
         if isinstance(constraint, List):
-            for each_constraint in constraint:
-                self.subject_to(each_constraint)
-            return
+            return [
+                self.subject_to(each_constraint) # return the dual of each constraint
+                for each_constraint in constraint
+            ]
 
         # If it's a proper constraint (MX type), pass it into the problem formulation and be done with it.
         if isinstance(constraint, cas.MX):
             super().subject_to(constraint)
-            return
+            dual = self.dual(constraint)
+
+            # Add the dual to the constraints list to be tracked
+            self.constraints_list.append(dual)
+
+            return dual
 
         # If the constraint(s) always evaluates True (e.g. if you enter "5 > 3"), skip it.
         # This allows you to toggle frozen variables without causing problems with setting up constraints.
         elif np.all(constraint):
-            return
+            return None # dual of an always-true constraint doesn't make sense to evaluate.
 
         # If any of the constraint(s) are always False (e.g. if you enter "5 < 3"), raise an error.
         # This indicates that the problem is infeasible as-written, likely because the user has frozen too
@@ -130,6 +178,26 @@ class Opti(cas.Opti):
             doesn't appear to be a symbolic type or a boolean type. You supplied the following constraint:
             {constraint}""")
 
+    def parameter(self,
+                  value: float,
+                  ) -> cas.MX:
+        """
+        Initialize a new parameter.
+
+        Args:
+            value: Value to set the parameter to.
+
+        Returns:
+            The parameter itself as a symbolic CasADi variable (MX type).
+
+        """
+        param = super().parameter()
+        self.set_value(param, value)
+
+        # Add the parameter to the parameter list to be tracked
+        self.parameter_list.append(param)
+
+        return param
 
 if __name__ == '__main__':
     pytest.main()
