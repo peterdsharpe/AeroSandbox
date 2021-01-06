d34cb61fe7c467d2f26854f3ebd352aa31c5cfd7
Peter Sharpe <peterdsharpe@gmail.com>
2021-01-05 20:49:34 -0500

    write a test for dependency import

---
+++ aerosandbox/test_dependencies.py
@@ -0,0 +1,15 @@
+import pytest
+
+def test_dependencies():
+    import numpy
+    import casadi
+    import plotly
+    import pandas
+    import matplotlib
+    import seaborn
+    import tqdm
+    import dill
+    import multiprocess
+
+if __name__ == '__main__':
+    pytest.main()
