45d64f0902270404b03ca63c377e754effa76a99
Peter Sharpe <peterdsharpe@gmail.com>
2021-01-08 11:50:06 -0500

    test commit

--- aerosandbox/optimization/test_opti_save_load.py
+++ aerosandbox/optimization/test_opti_save_load.py
@@ -135,4 +135,5 @@ def test_save_and_load_opti_vectorized(temp_filename=r"C:/Users/User/Downloads/t
 
 if __name__ == '__main__':
     pytest.main()
+    print("Hello world")
     # test_save_and_load_opti_vectorized()
