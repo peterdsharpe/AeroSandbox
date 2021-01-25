6dab999d8e4dc1e05355ed69d41da9c5dc68348f
Peter Sharpe <peterdsharpe@gmail.com>
2021-01-25 18:09:55 -0500

    add contain_points

--- aerosandbox/geometry/airfoil/test_airfoil.py
+++ aerosandbox/geometry/airfoil/test_airfoil.py
@@ -17,12 +17,21 @@ def test_TE_angle(naca4412):
 def test_local_thickness(e216):
     assert e216.local_thickness(0.5) == pytest.approx(0.08730287761717835)
 
-def LE_index_test(e216):
+def test_LE_index(e216):
     assert e216.LE_index() == 32
 
-def repanel_test(naca4412):
+def test_repanel(naca4412):
     naca4412.repanel(n_points_per_side=200)
     # assert
+
+def test_containts_points(naca4412):
+    assert naca4412.contains_points(
+        points=np.array([0.5, 0])
+    ) == True
+    assert np.all(naca4412.contains_points(
+        points=np.array([[0.5, 0], [0.5, -1]])
+    ) == np.array([True, False]))
+
 
 if __name__ == '__main__':
     pytest.main()
