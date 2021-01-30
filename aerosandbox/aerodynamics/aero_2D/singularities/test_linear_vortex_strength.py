763f3d5840e424e9bdcc0af74d8613b3c1d8faae
Peter Sharpe <peterdsharpe@gmail.com>
2021-01-29 23:49:40 -0500

    remade vortex tests

--- aerosandbox/aerodynamics/aero_2D/singularities/test_linear_vortex_strength.py
+++ aerosandbox/aerodynamics/aero_2D/singularities/test_linear_vortex_strength.py
@@ -1,5 +1,8 @@
 from aerosandbox.aerodynamics.aero_2D.singularities import *
+import numpy as np
+from numpy import pi
 import pytest
+
 
 def test_calculate_induced_velocity_panel_coordinates():
     X, Y = np.meshgrid(
@@ -10,139 +13,42 @@ def test_calculate_induced_velocity_panel_coordinates():
     X = X.flatten()
     Y = Y.flatten()
 
-    U, V = _calculate_induced_velocity_single_panel_panel_coordinates(
-        xp_field=X,
-        yp_field=Y,
-        backend="numpy"
-    )
-
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-
-    sns.set(palette=sns.color_palette("husl"))
-    fig, ax = plt.subplots(1, 1, figsize=(6.4, 4.8), dpi=200)
-
-    plt.quiver(
-        X, Y, U, V,
-        (U ** 2 + V ** 2) ** 0.5,
-        scale=5
-    )
-    plt.xlabel(r"$x$")
-    plt.ylabel(r"$z$")
-    plt.title(r"Linear-Strength Vortex: Induced Velocity")
-    plt.tight_layout()
-    # plt.savefig("C:/Users/User/Downloads/temp.svg")
-    plt.show()
-
-
-def test_calculate_induced_velocity():
-    X, Y = np.meshgrid(
-        np.linspace(-1, 2, 50),
-        np.linspace(-1, 1, 50),
-        indexing='ij',
-    )
-    X = X.flatten()
-    Y = Y.flatten()
-
-    U, V = _calculate_induced_velocity_single_panel(
+    U, V = calculate_induced_velocity_line_singularities(
         x_field=X,
         y_field=Y,
-        x_panel_start=0.5,
-        y_panel_start=0,
-        x_panel_end=1,
-        y_panel_end=1,
-        backend="numpy"
+        x_panels=np.array([-0.5, 1.5]),
+        y_panels=np.array([0, 0]),
+        gamma=np.array([1, 1]),
+        sigma=np.array([1, 1]),
     )
 
-    import matplotlib.pyplot as plt
-    import seaborn as sns
 
-    sns.set(palette=sns.color_palette("husl"))
-    fig, ax = plt.subplots(1, 1, figsize=(6.4, 4.8), dpi=200)
-
-    plt.quiver(
-        X, Y, U, V,
-        (U ** 2 + V ** 2) ** 0.5,
-        scale=5
+def test_vortex_limit():
+    eps = 1e-6
+    u, v = calculate_induced_velocity_line_singularities(
+        x_field=1,
+        y_field=0,
+        x_panels=[0, eps],
+        y_panels=[0, 0],
+        gamma=[1 / eps, 1 / eps],
+        sigma=[0, 0],
     )
-    plt.xlabel(r"$x$")
-    plt.ylabel(r"$z$")
-    plt.title(r"Linear-Strength Vortex: Induced Velocity")
-    plt.tight_layout()
-    # plt.savefig("C:/Users/User/Downloads/temp.svg")
-    plt.show()
+    assert u == pytest.approx(0, abs=eps ** 0.5)
+    assert v == pytest.approx(-1 / (2 * pi), abs=eps ** 0.5)
 
 
-def test_calculate_induced_velocity_panel_coordinates_casadi():
-    X, Y = np.meshgrid(
-        np.linspace(-1, 2, 50),
-        np.linspace(-1, 1, 50),
-        indexing='ij',
+def test_source_limit():
+    eps = 1e-6
+    u, v = calculate_induced_velocity_line_singularities(
+        x_field=1,
+        y_field=0,
+        x_panels=[0, eps],
+        y_panels=[0, 0],
+        sigma=[1 / eps, 1 / eps],
+        gamma=[0, 0],
     )
-    X = X.flatten()
-    Y = Y.flatten()
-
-    U, V = _calculate_induced_velocity_single_panel_panel_coordinates(
-        xp_field=X,
-        yp_field=Y,
-        backend="casadi"
-    )
-
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-
-    sns.set(palette=sns.color_palette("husl"))
-    fig, ax = plt.subplots(1, 1, figsize=(6.4, 4.8), dpi=200)
-
-    plt.quiver(
-        X, Y, U, V,
-        (U ** 2 + V ** 2) ** 0.5,
-        scale=5
-    )
-    plt.xlabel(r"$x$")
-    plt.ylabel(r"$z$")
-    plt.title(r"Linear-Strength Vortex: Induced Velocity")
-    plt.tight_layout()
-    # plt.savefig("C:/Users/User/Downloads/temp.svg")
-    plt.show()
-
-
-def test_calculate_induced_velocity_casadi():
-    X, Y = np.meshgrid(
-        np.linspace(-1, 2, 50),
-        np.linspace(-1, 1, 50),
-        indexing='ij',
-    )
-    X = X.flatten()
-    Y = Y.flatten()
-
-    U, V = _calculate_induced_velocity_single_panel(
-        x_field=X,
-        y_field=Y,
-        x_panel_start=0.5,
-        y_panel_start=0,
-        x_panel_end=1,
-        y_panel_end=1,
-        backend="casadi"
-    )
-
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-
-    sns.set(palette=sns.color_palette("husl"))
-    fig, ax = plt.subplots(1, 1, figsize=(6.4, 4.8), dpi=200)
-
-    plt.quiver(
-        X, Y, U, V,
-        (U ** 2 + V ** 2) ** 0.5,
-        scale=5
-    )
-    plt.xlabel(r"$x$")
-    plt.ylabel(r"$z$")
-    plt.title(r"Linear-Strength Vortex: Induced Velocity")
-    plt.tight_layout()
-    # plt.savefig("C:/Users/User/Downloads/temp.svg")
-    plt.show()
+    assert u == pytest.approx(1 / (2 * pi), abs=eps ** 0.5)
+    assert v == pytest.approx(0, abs=eps ** 0.5)
 
 
 if __name__ == '__main__':
