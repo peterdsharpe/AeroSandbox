325d7e5de68ee59c99bc07b43223d45e08f5fe94
Peter Sharpe <peterdsharpe@gmail.com>
2021-01-30 10:43:37 -0500

    make Atmosphere based operatingpoint

---
+++ aerosandbox/performance/__init__.py
@@ -0,0 +1,1 @@
+from .operating_point import *
--- aerosandbox/performance/operating_point.py
+++ aerosandbox/performance/operating_point.py
@@ -1,23 +1,22 @@
 import casadi as cas
 from aerosandbox.geometry import *
+from aerosandbox import Atmosphere
+from numpy import pi
 
 
 class OperatingPoint(AeroSandboxObject):
     def __init__(self,
-                 density=1.225,  # kg/m^3
-                 viscosity=1.81e-5,  # kg/m-s
-                 velocity=10,  # m/s
-                 mach=0,  # Freestream mach number
-                 alpha=5,  # In degrees
-                 beta=0,  # In degrees
-                 p=0,  # About the body x-axis, in rad/sec
-                 q=0,  # About the body y-axis, in rad/sec
-                 r=0,  # About the body z-axis, in rad/sec
+                 atmosphere: Atmosphere = Atmosphere(altitude=0),
+                 velocity: float = 10.,  # m/s
+                 alpha: float = 5.,  # In degrees
+                 beta: float = 0.,  # In degrees
+                 p: float = 0.,  # About the body x-axis, in rad/sec
+                 q: float = 0.,  # About the body y-axis, in rad/sec
+                 r: float = 0.,  # About the body z-axis, in rad/sec
                  ):
-        self.density = density
-        self.viscosity = viscosity
+        self.atmosphere = atmosphere
+        self.altitude = altitude
         self.velocity = velocity
-        self.mach = mach
         self.alpha = alpha
         self.beta = beta
         self.p = p
@@ -35,12 +34,29 @@ class OperatingPoint(AeroSandboxObject):
         """
         return 0.5 * self.density * self.velocity ** 2
 
+    def reynolds(self, reference_length):
+        """
+        Computes a Reynolds number with respect to a given reference length.
+        :param reference_length: A reference length you choose [m]
+        :return: Reynolds number [unitless]
+        """
+        density = self.atmosphere.density()
+        viscosity = self.atmosphere.dynamic_viscosity()
+
+        return density * self.velocity * reference_length / viscosity
+
+    def mach(self):
+        """
+        Return the mach number associated with the current flight condition.
+        """
+        return self.velocity / self.atmosphere.speed_of_sound()
+
     def compute_rotation_matrix_wind_to_geometry(self):
         # Computes the 3x3 rotation matrix required to go from wind axes to geometry axes.
-        sinalpha = cas.sin(self.alpha * cas.pi / 180)
-        cosalpha = cas.cos(self.alpha * cas.pi / 180)
-        sinbeta = cas.sin(self.beta * cas.pi / 180)
-        cosbeta = cas.cos(self.beta * cas.pi / 180)
+        sinalpha = np.sin(self.alpha * pi / 180)
+        cosalpha = np.cos(self.alpha * pi / 180)
+        sinbeta = np.sin(self.beta * pi / 180)
+        cosbeta = np.cos(self.beta * pi / 180)
 
         # r=-1*cas.array([
         #     [cosbeta*cosalpha, -sinbeta, cosbeta*sinalpha],
@@ -102,49 +118,3 @@ class OperatingPoint(AeroSandboxObject):
         rotation_velocity_geometry_axes = -rotation_velocity_geometry_axes  # negative sign, since we care about the velocity the WING SEES, not the velocity of the wing.
 
         return rotation_velocity_geometry_axes
-
-    def compute_reynolds(self, reference_length):
-        """
-        Computes a reynolds number with respect to a given reference length.
-        :param reference_length: A reference length you choose [m]
-        :return: Reynolds number [unitless]
-        """
-        return self.density * self.velocity * reference_length / self.viscosity
-
-
-class AeroData: # TODO finish this class
-    # A class where aerodynamic data is stored.
-    # There is very little structure here. Suggested attributes you could add here:
-
-    # # Forces / Moments
-    # force_wind_axes,
-    # force_geometry_axes,
-    # CL,
-    # CD,
-    # CY,
-    # Cl,
-    # Cm,
-    # Cn,
-
-    # # Stability Derivatives
-
-    def __init__(self,
-                 force_wind_axes=None,
-                 force_geometry_axes=None,
-                 CL=None,
-                 CD=None,
-                 CY=None,
-                 Cl=None,
-                 Cm=None,
-                 Cn=None,
-                 stability_jacobian=None
-                 ):
-        self.force_wind_axes = force_wind_axes
-        self.force_geometry_axes = force_geometry_axes
-        self.CL = CL
-        self.CD = CD
-        self.CY = CY
-        self.Cl = Cl
-        self.Cm = Cm
-        self.Cn = Cn
-        self.stability_jacobian = stability_jacobian
