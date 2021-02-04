37f5a86afcc43860748aa8daede431a006903f66
Peter Sharpe <peterdsharpe@gmail.com>
2021-02-04 00:20:10 -0500

    add mean() function.

--- aerosandbox/optimization/math/arithmetic.py
+++ aerosandbox/optimization/math/arithmetic.py
@@ -1,5 +1,6 @@
 import numpy as np
 import casadi as cas
+from aerosandbox.optimization.math.array import length
 
 
 def sum1(x):
@@ -8,3 +9,8 @@ def sum1(x):
         return np.sum(x)
     except Exception:
         return cas.sum1(x)
+
+
+def mean(x):
+    """Returns the mean of a vector x."""
+    return sum1(x) / length(x)
