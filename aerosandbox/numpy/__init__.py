6514d95082b3d82bb4a3a4f196ccce9451597583
Peter Sharpe <peterdsharpe@gmail.com>
2021-02-07 00:25:05 -0500

    force numpy to overwrite round, min, max

--- aerosandbox/numpy/__init__.py
+++ aerosandbox/numpy/__init__.py
@@ -1,5 +1,8 @@
+### Import everything from NumPy
+
 from numpy import *
 
+### Overwrite some functions
 from .array import array, length
 from .arithmetic import sum, mean
 from .calculus import diff, trapz
@@ -10,3 +13,11 @@ from .rotations import rotation_matrix_2D, rotation_matrix_3D
 from .spacing import linspace, cosspace
 from .surrogate_model_tools import smoothmax, sigmoid, blend
 from .trig import degrees, radians, sind, cosd, tand, arcsind, arccosd, arctan2d
+
+### Force-overwrite built-in Python functions.
+
+from numpy import round, min, max
+
+
+def abs(x):
+    return fabs(x)
