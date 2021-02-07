7d859dd7572d2213b1861e4acbaa83dc82dd7c31
Peter Sharpe <peterdsharpe@gmail.com>
2021-02-07 00:24:45 -0500

    refactor linear_strength_line_singularities.py for numpy compatibility

--- aerosandbox/aerodynamics/aero_2D/singularities/linear_strength_line_singularities.py
+++ aerosandbox/aerodynamics/aero_2D/singularities/linear_strength_line_singularities.py
@@ -1,8 +1,6 @@
-from numpy import pi
 import aerosandbox.numpy as np
-from aerosandbox import cas
 from typing import Union
-
+import casadi as cas
 
 def _calculate_induced_velocity_line_singularity_panel_coordinates(
         xp_field: Union[float, np.ndarray],
@@ -12,7 +10,6 @@ def _calculate_induced_velocity_line_singularity_panel_coordinates(
         sigma_start: float = 0.,
         sigma_end: float = 0.,
         xp_panel_end: float = 1.,
-        backend: str = "numpy", # TODO rewrite to deprecate this based on aerosandbox.numpy overloading
 ) -> [Union[float, np.ndarray], Union[float, np.ndarray]]:
     """
     Calculates the induced velocity at a point (xp_field, yp_field) in a 2D potential-flow flowfield.
@@ -33,9 +30,6 @@ def _calculate_induced_velocity_line_singularity_panel_coordinates(
     Inputs x and y can be 1D ndarrays representing various field points,
     in which case the resulting velocities u and v have corresponding dimensionality.
 
-    The `backend` parameter selects a numerical backend to be used, with the options "numpy" and "casadi".
-    NumPy is significantly faster, but CasADi is differentiable.
-
     Equations from the seminal textbook "Low Speed Aerodynamics" by Katz and Plotkin.
     Vortex equations are Eq. 11.99 and Eq. 11.100.
         * Note: there is an error in equation 11.100 in Katz and Plotkin, at least in the 2nd ed:
@@ -46,10 +40,6 @@ def _calculate_induced_velocity_line_singularity_panel_coordinates(
     Source equations are Eq. 11.89 and Eq. 11.90.
 
     """
-    ### Validate inputs
-    if not (backend == "numpy" or backend == "casadi"):
-        raise ValueError("Bad value of 'backend'!")
-
     ### Modify any incoming floats
     if isinstance(xp_field, (float, int)):
         xp_field = np.array([xp_field])
@@ -57,18 +47,14 @@ def _calculate_induced_velocity_line_singularity_panel_coordinates(
         yp_field = np.array([yp_field])
 
     ### Determine if you can skip either the vortex or source parts
-    if backend == "numpy":
-        skip_vortex_math = gamma_start == 0 and gamma_end == 0
-        skip_source_math = sigma_start == 0 and sigma_end == 0
-    elif backend == "casadi":
-        skip_vortex_math = not (
-                isinstance(gamma_start, cas.MX) or
-                isinstance(gamma_end, cas.MX)
-        ) and gamma_start == 0 and gamma_end == 0
-        skip_source_math = not (
-                isinstance(sigma_start, cas.MX) or
-                isinstance(sigma_end, cas.MX)
-        ) and sigma_start == 0 and sigma_end == 0
+    skip_vortex_math = not (
+            isinstance(gamma_start, cas.MX) or
+            isinstance(gamma_end, cas.MX)
+    ) and gamma_start == 0 and gamma_end == 0
+    skip_source_math = not (
+            isinstance(sigma_start, cas.MX) or
+            isinstance(sigma_end, cas.MX)
+    ) and sigma_start == 0 and sigma_end == 0
 
     ### Determine which points are effectively on the panel, necessitating different math:
     is_on_panel = np.fabs(yp_field) <= 1e-8
@@ -86,7 +72,13 @@ def _calculate_induced_velocity_line_singularity_panel_coordinates(
     theta_2 = np.arctan2(yp_field, xp_field - xp_panel_end)
     ln_r_2_r_1 = np.log(r_2 / r_1)
     d_theta = theta_2 - theta_1
-    tau = 2 * pi
+    tau = 2 * np.pi
+
+    yp_field_regularized = np.where(
+        is_on_panel,
+        1,
+        yp_field
+    )
 
     ### VORTEX MATH
     if skip_vortex_math:
@@ -111,39 +103,24 @@ def _calculate_induced_velocity_line_singularity_panel_coordinates(
         u_vortex = u_vortex_term_1 + u_vortex_term_2
 
         # Correct the u-velocity if field point is on the panel
-        if backend == "numpy":
-            u_vortex[is_on_panel] = 0
-        elif backend == "casadi":
-            u_vortex = cas.if_else(
-                is_on_panel,
-                0,
-                u_vortex
-            )
+        u_vortex = np.where(
+            is_on_panel,
+            0,
+            u_vortex
+        )
 
         # Calculate v_vortex
         v_vortex_term_1 = u_vortex_term_2_quantity * ln_r_2_r_1
 
-        if backend == "numpy":  # This is basically an optimized version of np.where
-            v_vortex_term_2 = np.empty_like(v_vortex_term_1)
-            v_vortex_term_2[~is_on_panel] = u_vortex_term_1_quantity[~is_on_panel] * (
-                    xp_panel_end / yp_field[~is_on_panel] -
-                    d_theta[~is_on_panel]
-            )
-            v_vortex_term_2[is_on_panel] = d_gamma / tau
-        elif backend == "casadi":
-            yp_field_regularized = cas.if_else(
-                is_on_panel,
-                1,
-                yp_field
-            )
-            v_vortex_term_2 = cas.if_else(
-                is_on_panel,
-                d_gamma / tau,
-                u_vortex_term_1_quantity * (
-                        xp_panel_end / yp_field_regularized -
-                        d_theta
-                ),
-            )
+
+        v_vortex_term_2 = np.where(
+            is_on_panel,
+            d_gamma / tau,
+            u_vortex_term_1_quantity * (
+                    xp_panel_end / yp_field_regularized -
+                    d_theta
+            ),
+        )
 
         v_vortex = v_vortex_term_1 + v_vortex_term_2
 
@@ -169,34 +146,23 @@ def _calculate_induced_velocity_line_singularity_panel_coordinates(
         v_source = v_source_term_1 + v_source_term_2
 
         # Correct the v-velocity if field point is on the panel
-        if backend == "numpy":
-            v_source[is_on_panel] = 0
-        elif backend == "casadi":
-            v_source = cas.if_else(
-                is_on_panel,
-                0,
-                v_source
-            )
+        v_source = np.where(
+            is_on_panel,
+            0,
+            v_source
+        )
 
         # Calculate u_source
         u_source_term_1 = -v_source_term_2_quantity * ln_r_2_r_1
 
-        if backend == "numpy":  # This is basically an optimized version of np.where
-            u_source_term_2 = np.empty_like(u_source_term_1)
-            u_source_term_2[~is_on_panel] = -v_source_term_1_quantity[~is_on_panel] * (
-                    xp_panel_end / yp_field[~is_on_panel] -
-                    d_theta[~is_on_panel]
-            )
-            u_source_term_2[is_on_panel] = -d_sigma / tau
-        elif backend == "casadi":
-            u_source_term_2 = cas.if_else(
-                is_on_panel,
-                -d_sigma / tau,
-                -v_source_term_1_quantity * (
-                        xp_panel_end / yp_field_regularized -
-                        d_theta
-                ),
-            )
+        u_source_term_2 = np.where(
+            is_on_panel,
+            -d_sigma / tau,
+            -v_source_term_1_quantity * (
+                    xp_panel_end / yp_field_regularized -
+                    d_theta
+            ),
+        )
 
         u_source = u_source_term_1 + u_source_term_2
 
@@ -217,7 +183,6 @@ def _calculate_induced_velocity_line_singularity(
         gamma_end: float = 0.,
         sigma_start: float = 0.,
         sigma_end: float = 0.,
-        backend: str = "numpy"
 ) -> [Union[float, np.ndarray], Union[float, np.ndarray]]:
     """
     Calculates the induced velocity at a point (x_field, y_field) in a 2D potential-flow flowfield.
@@ -234,9 +199,6 @@ def _calculate_induced_velocity_line_singularity(
 
     Inputs x and y can be 1D ndarrays representing various field points,
     in which case the resulting velocities u and v have the corresponding dimensionality.
-
-    The `backend` parameter selects a numerical backend to be used, with the options "numpy" and "casadi".
-    NumPy is significantly faster, but CasADi is differentiable.
 
     """
     ### Calculate the panel coordinate transform (x -> xp, y -> yp), where
@@ -266,7 +228,6 @@ def _calculate_induced_velocity_line_singularity(
         sigma_start=sigma_start,
         sigma_end=sigma_end,
         xp_panel_end=panel_length,
-        backend=backend
     )
 
     ### Transform the velocities in panel coordinates back to global coordinates
@@ -284,7 +245,6 @@ def calculate_induced_velocity_line_singularities(
         y_panels: np.ndarray,
         gamma: np.ndarray,
         sigma: np.ndarray,
-        backend: str = "numpy",
 ) -> [Union[float, np.ndarray], Union[float, np.ndarray]]:
     """
     Calculates the induced velocity at a point (x_field, y_field) in a 2D potential-flow flowfield.
@@ -301,9 +261,6 @@ def calculate_induced_velocity_line_singularities(
 
     Inputs x and y can be 1D ndarrays representing various field points,
     in which case the resulting velocities u and v have the corresponding dimensionality.
-
-    The `backend` parameter selects a numerical backend to be used, with the options "numpy" and "casadi".
-    NumPy is significantly faster, but CasADi is differentiable.
 
     """
     try:
@@ -323,7 +280,6 @@ def calculate_induced_velocity_line_singularities(
             gamma_end=gamma[i + 1],
             sigma_start=sigma[i],
             sigma_end=sigma[i + 1],
-            backend=backend
         )
         if i == 0:
             u_field = u
