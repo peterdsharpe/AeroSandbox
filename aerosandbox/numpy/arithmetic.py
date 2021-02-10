695e2abc848da97266cc487c82bf608978a31417
Peter Sharpe <peterdsharpe@gmail.com>
2021-02-10 18:26:28 -0500

    Fix type checking on sum

--- aerosandbox/numpy/arithmetic.py
+++ aerosandbox/numpy/arithmetic.py
@@ -1,14 +1,15 @@
 import numpy as onp
 import casadi as cas
 from aerosandbox.numpy.array import length
+from .determine_type import is_casadi_type
 
 
 def sum(x):
     """Returns the sum of a vector x."""
-    try:
-        return onp.sum(x)
-    except Exception:
+    if is_casadi_type(x):
         return cas.sum1(x)
+    else:
+        return onp.sum(x)
 
 
 def mean(x):
