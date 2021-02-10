3599fbf3b21bf8b6f0433f4a25ca597767ae0ca5
Peter Sharpe <peterdsharpe@gmail.com>
2021-02-10 18:39:55 -0500

    docs

--- tutorial/1 - Optimization and Math/3 - Sphere function, vectorized.py
+++ tutorial/1 - Optimization and Math/3 - Sphere function, vectorized.py
@@ -36,3 +36,32 @@ x_opt = sol.value(x)
 
 # Print values
 print(f"x = {x_opt}")
+
+"""
+We find the solution of this optimization problem to be a vector of 100 zeroes - makes sense.
+
+Note that because this is a special case called a "quadratic program" and we're using a modern second-order optimizer 
+(IPOPT) as the backend, this solves in just one iteration.
+
+Let's talk a bit more about vectorized variables. We demonstrated that one can create a vectorized variable with the 
+syntax:
+
+>>> x = opti.variable(
+>>>     init_guess=np.ones(shape=N)
+>>> )
+
+One can also use the syntax:
+
+>>> x = opti.variable(
+>>>     init_guess=1,
+>>>     n_vars=N,
+>>> )
+
+Which will also initialize a vector variable of length N with initial guess of 1. Of course, let's say that you 
+wanted to initialize each element of the vector x to a different value; say, something like np.linspace(0, 1, 
+N). Then, you would have to use the former syntax rather than the latter. 
+
+Note that when solving high-dimensional, nonlinear, nonconvex systems, it is very, very important to provide an 
+initial guess that is as close to accurate as possible! This is true for both scalar and vector variables. 
+
+"""
