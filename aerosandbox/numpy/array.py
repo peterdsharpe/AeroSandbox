92b28955dae0248a4642e7b12aaf08a1261b223f
Peter Sharpe <peterdsharpe@gmail.com>
2021-02-11 23:23:59 -0500

    in array.py, catch attribute error

--- aerosandbox/numpy/array.py
+++ aerosandbox/numpy/array.py
@@ -3,25 +3,25 @@ import casadi as cas
 
 
 def array(object, dtype=None):
-    a = onp.array(object, dtype=dtype)
-
-    if a.dtype != "O":  # If it's not an object array, then you're done here!
+    try:
+        a = onp.array(object, dtype=dtype)
+        if a.dtype == "O":
+            raise AttributeError
         return a
+    except AttributeError: # If this occurs, it needs to be a CasADi type.
+        # First, determine the dimension
+        def make_row(row):
+            try:
+                return cas.horzcat(*row)
+            except (TypeError, Exception):  # If not iterable or if it's a CasADi MX type
+                return row
 
-    ### If it is an object array, then we need to make it into a CasADi matrix instead.
-    # First, determine the dimension
-    def make_row(row):
-        try:
-            return cas.horzcat(*row)
-        except (TypeError, Exception):  # If not iterable or if it's a CasADi MX type
-            return row
-
-    return cas.vertcat(
-        *[
-            make_row(row)
-            for row in object
-        ]
-    )
+        return cas.vertcat(
+            *[
+                make_row(row)
+                for row in object
+            ]
+        )
 
 
 def length(array) -> int:
