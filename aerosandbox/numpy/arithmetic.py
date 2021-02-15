7d60f05ac61d667f9577b77550dd1dbd1c5d5cea
Christophe Foyer <christophe.aa.foyer@gmail.com>
2021-02-15 23:21:35 +0000

    Fixed sum and polygon with it

--- aerosandbox/numpy/arithmetic.py
+++ aerosandbox/numpy/arithmetic.py
@@ -3,12 +3,17 @@ import casadi as cas
 from aerosandbox.numpy.array import length
 
 
-def sum(x):
+def sum(x, axis=0):
     """Returns the sum of a vector x."""
     try:
-        return onp.sum(x)
-    except Exception:
-        return cas.sum1(x)
+        return onp.sum(x, axis)
+    except Exception:  # TODO: Check behavior
+        if axis==0:
+            return cas.sum1(x.T).T
+        if axis==1:
+            return cas.sum1(x)
+        else:
+            raise
 
 
 def mean(x):
