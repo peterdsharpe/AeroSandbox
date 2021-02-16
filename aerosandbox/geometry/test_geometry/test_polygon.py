331f3533d255aad827c53b9908b5bc296486a544
Christophe Foyer <christophe.aa.foyer@gmail.com>
2021-02-16 12:48:07 +0000

    Fixed arithmetics issue, caused new issues
    story of my life, but hopefully moving in the right direction. needs more testing

--- aerosandbox/geometry/test_geometry/test_polygon.py
+++ aerosandbox/geometry/test_geometry/test_polygon.py
@@ -12,6 +12,7 @@ xy = np.flip(np.array([
 x = xy[:, 0]
 y = xy[:, 1]
 
+
 def test_area1():
     poly = Polygon(x, y)
     
@@ -19,12 +20,14 @@ def test_area1():
     assert all(poly.y() == y)
     assert poly.area() == 1
 
+
 def test_area2():
     poly = Polygon(xy)
     
     assert all(poly.x() == x)
     assert all(poly.y() == y)
     assert poly.area() == 1
+    
     
 def test_area3():
     x = np.vstack([xy[:, 0]]*100)
@@ -35,6 +38,27 @@ def test_area3():
     assert np.all(poly.x() == x)
     assert np.all(poly.y() == y)
     assert np.all(poly.area() == np.ones((100,)))
+    
+    
+def contains_point1():
+    poly = Polygon(xy)
+    
+    assert poly.contains_points(0.5, 0.5)
+    
+    
+def centroid1():
+    poly = Polygon(xy)
+    
+    assert poly.centroid() == np.array([0.5, 0.5])
+    
+    
+def I_J():
+    poly = Polygon(xy)
+    
+    assert np.isclose(poly.Ixx(), 0.08333333333333331)
+    assert np.isclose(poly.Iyy(), 0.08333333333333331)
+    assert np.isclose(poly.Ixy(), 0)
+    assert np.isclose(poly.J(), 0.16666666666666663)
     
 
 if __name__ == '__main__':
