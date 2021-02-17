539dadbbc547acf04e391cd0dc83b6788be1d05b
Peter Sharpe <peterdsharpe@gmail.com>
2021-02-17 15:10:35 -0500

    Add jupyter support in test_all_tutorials.py

--- tutorial/test_all_tutorials.py
+++ tutorial/test_all_tutorials.py
@@ -5,11 +5,39 @@ to ensure that they don't throw any errors).
 
 import os
 from pathlib import Path
+import tempfile
+import json
+
+
+def convert_ipynb_to_py(
+        input_file: str,
+        output_file: str,
+):
+    """
+    Reads an input Jupyter notebook (.ipynb) and converts it to a Python file (.py)
+
+    Tried using `jupyter nbconvert`, but that is SO SLOW, like 3 seconds per notebook! It's just json parsing,
+    this should *not* take more than a few milliseconds - come on, Jupyter!
+
+    Args:
+        input_file: File path, including ".ipynb"
+        output_file: File path, including ".py"
+
+    Returns: None
+
+    """
+    with open(input_file, "r") as f:
+        ipynb_contents = json.load(f)
+    with open(output_file, "w+") as f:
+        for cell in ipynb_contents['cells']:
+            if cell['cell_type'] == "code":
+                f.writelines(cell['source'])
+                f.write("\n")
 
 
 def run_all_python_files(path: Path, recursive=True) -> None:
     """
-    Executes all Python files in a directory.
+    Executes all Python files and Jupyter Notebooks in a directory.
     Args:
         path: A Path-type object (Path from built-in pathlib) representing a filepath
         recursive: Executes recursively (e.g. searches all subfolders too)
@@ -23,9 +51,26 @@ def run_all_python_files(path: Path, recursive=True) -> None:
     if "Tutorials in Progress - Ignore for Now" in str(path):  # Don't run this folder
         return
 
-    ### Run the file if it's a Python file
-    if path.is_file() and path.suffix == ".py":
-        exec(open(str(path)).read())
+    if path.is_file():
+
+        ### Run the file if it's a Python file
+        if path.suffix == ".py":
+            exec(open(str(path)).read())
+
+        ### Run the file if it's a Jupyter notebook
+        if path.suffix == ".ipynb":
+            with tempfile.TemporaryDirectory() as tempdir:
+                notebook = path
+                output_dir = tempdir
+                output_filename = path.name.strip(path.suffix) + ".py"
+                python_file = output_dir + os.sep + output_filename
+
+                # convert_command = f'jupyter nbconvert --to python "{notebook}" --output-dir "{output_dir}" --output "{output_filename}"'
+                # os.system(convert_command)
+
+                convert_ipynb_to_py(notebook, python_file)
+
+                exec(open(python_file).read())
 
     ### Recurse through a directory if directed to
     if recursive and path.is_dir():
@@ -42,5 +87,4 @@ if __name__ == '__main__':
     # test_all_tutorials()
 
     import pytest
-
     pytest.main()
