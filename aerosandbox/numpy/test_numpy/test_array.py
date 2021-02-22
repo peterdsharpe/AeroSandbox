c335a858ab2101b6d91ca788a0b159e3e3f22767
Peter Sharpe <peterdsharpe@gmail.com>
2021-02-22 16:49:23 -0500

    add tests for new array stacking features

--- aerosandbox/numpy/test_numpy/test_array.py
+++ aerosandbox/numpy/test_numpy/test_array.py
@@ -4,7 +4,7 @@ import aerosandbox.numpy as np
 import casadi as cas
 
 
-def test_numpy_equivalency_1D():
+def test_array_numpy_equivalency_1D():
     inputs = [
         1,
         2
@@ -16,7 +16,7 @@ def test_numpy_equivalency_1D():
     assert np.all(a == a_np)
 
 
-def test_numpy_equivalency_2D():
+def test_array_numpy_equivalency_2D():
     inputs = [
         [1, 2],
         [3, 4]
@@ -28,8 +28,9 @@ def test_numpy_equivalency_2D():
     assert np.all(a == a_np)
 
 
-def test_casadi_1D_shape():
+def test_array_casadi_1D_shape():
     a = array([cas.DM(1), cas.DM(2)])
+    assert length(a) == 2
 
 
 def test_length():
@@ -43,6 +44,37 @@ def test_length():
     assert length(np.ones((3, 2))) == 3
 
     assert length(cas.GenMX_ones(5)) == 5
+
+
+def test_concatenate():
+    n = np.arange(10)
+    c = cas.DM(n)
+
+    assert concatenate((n, n)).shape == (20,)
+    assert concatenate((n, c)).shape == (20, 1)
+    assert concatenate((c, n)).shape == (20, 1)
+    assert concatenate((c, c)).shape == (20, 1)
+
+    assert concatenate((n, n, n)).shape == (30,)
+    assert concatenate((c, c, c)).shape == (30, 1)
+
+
+def test_stack():
+    n = np.arange(10)
+    c = cas.DM(n)
+
+    assert stack((n, n)).shape == (2, 10)
+    assert stack((n, n), axis=-1).shape == (10, 2)
+    assert stack((n, c)).shape == (2, 10)
+    assert stack((n, c), axis=-1).shape == (10, 2)
+    assert stack((c, c)).shape == (2, 10)
+    assert stack((c, c), axis=-1).shape == (10, 2)
+
+    with pytest.raises(Exception):
+        assert stack((n, n), axis=2)
+
+    with pytest.raises(Exception):
+        stack((c, c), axis=2)
 
 
 if __name__ == '__main__':
