e67f6f1cc8650506429152695a437aa2156bff0c
Peter Sharpe <peterdsharpe@gmail.com>
2021-02-22 17:29:37 -0500

    made linspace(), cosspace() more robust. Added logspace() and geomspace().

--- aerosandbox/numpy/spacing.py
+++ aerosandbox/numpy/spacing.py
@@ -1,40 +1,77 @@
 import numpy as onp
 import casadi as cas
-
+from aerosandbox.numpy.determine_type import is_casadi_type
 
 
 def linspace(
         start: float = 0.,
         stop: float = 1.,
-        n_points: int = 50
+        num: int = 50
 ):
     """
-    Makes a linearly-spaced vector.
-    Args:
-        start: Value to start at.
-        stop: Value to end at.
-        n_points: Number of points in the vector.
+    Returns evenly spaced numbers over a specified interval.
+
+    See syntax here: https://numpy.org/doc/stable/reference/generated/numpy.linspace.html
     """
-    try:
-        return onp.linspace(start, stop, n_points)
-    except Exception:
-        return cas.linspace(start, stop, n_points)
+    if not is_casadi_type([start, stop, num], recursive=True):
+        return onp.linspace(start, stop, num)
+    else:
+        return cas.linspace(start, stop, num)
 
 
 def cosspace(
         start: float = 0.,
         stop: float = 1.,
-        n_points: int = 50
+        num: int = 50
 ):
     """
     Makes a cosine-spaced vector.
 
-    To learn about cosine spacing, see this: https://youtu.be/VSvsVgGbN7I
+    Cosine spacing is useful because these correspond to Chebyshev nodes: https://en.wikipedia.org/wiki/Chebyshev_nodes
+
+    To learn more about cosine spacing, see this: https://youtu.be/VSvsVgGbN7I
+
     Args:
         start: Value to start at.
         end: Value to end at.
-        n_points: Number of points in the vector.
+        num: Number of points in the vector.
     """
     mean = (stop + start) / 2
     amp = (stop - start) / 2
-    return mean + amp * onp.cos(onp.linspace(onp.pi, 0, n_points))
+    return mean + amp * onp.cos(linspace(onp.pi, 0, num))
+
+
+def logspace(
+        start: float = 0.,
+        stop: float = 1.,
+        num: int = 50
+):
+    """
+    Return numbers spaced evenly on a log scale.
+
+    See syntax here: https://numpy.org/doc/stable/reference/generated/numpy.logspace.html
+    """
+    if not is_casadi_type([start, stop, num], recursive=True):
+        return onp.logspace(start, stop, num)
+    else:
+        return 10 ** linspace(start, stop, num)
+
+
+def geomspace(
+        start: float = 1.,
+        stop: float = 10.,
+        num: int = 50
+):
+    """
+    Return numbers spaced evenly on a log scale (a geometric progression).
+
+    This is similar to logspace, but with endpoints specified directly.
+
+    See syntax here: https://numpy.org/doc/stable/reference/generated/numpy.geomspace.html
+    """
+    if not is_casadi_type([start, stop, num], recursive=True):
+        return onp.geomspace(start, stop, num)
+    else:
+        if start <= 0 or stop <= 0:
+            raise ValueError("Both start and stop must be positive!")
+        return onp.log10(10 ** linspace(start, stop, num))
