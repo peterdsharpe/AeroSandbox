2bbdf675fbf06fc73495fd898ec07d316a20afbe
Peter Sharpe <peterdsharpe@gmail.com>
2021-02-22 17:14:23 -0500

    Added support for the `axis` argument of sum()

--- aerosandbox/numpy/arithmetic.py
+++ aerosandbox/numpy/arithmetic.py
@@ -4,17 +4,29 @@ from aerosandbox.numpy.array import length
 from .determine_type import is_casadi_type
 
 
-def sum(x):
-    """Returns the sum of a vector x."""
-    if is_casadi_type(x):
-        return cas.sum1(x)
+def sum(x, axis: int = None):
+    """
+    Returns the sum of x.
+
+    See syntax here: https://numpy.org/doc/stable/reference/generated/numpy.sum.html
+    """
+    if not is_casadi_type(x):
+        return onp.sum(x, axis=axis)
     else:
-        return onp.sum(x)
+        if axis == 0:
+            return cas.sum1(x)
+        elif axis == 1:
+            return cas.sum2(x)
+        elif axis is None:
+            return sum(sum(x, axis=0), axis=1)
+        else:
+            raise ValueError("CasADi types can only be up to 2D, so `axis` must be None, 0, or 1.")
 
 
 def mean(x):
     """Returns the mean of a vector x."""
     return sum(x) / length(x)
+
 
 def abs(x):
     try:
