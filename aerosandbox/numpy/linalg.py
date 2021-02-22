c0f1b481db53e7fff1e72912b8b88dfe33dc69b7
Peter Sharpe <peterdsharpe@gmail.com>
2021-02-22 18:55:42 -0500

    rewrite norm() to be able to do tons of different norm types

--- aerosandbox/numpy/linalg.py
+++ aerosandbox/numpy/linalg.py
@@ -1,6 +1,8 @@
 import numpy as _onp
 import casadi as _cas
+from aerosandbox.numpy.arithmetic import sum, abs
 from aerosandbox.numpy.determine_type import is_casadi_type
+
 
 def inner(x, y):
     """Return the inner product of vectors x and y."""
@@ -41,12 +43,39 @@ def solve(A, b):  # TODO get this working
         return _cas.solve(A, b)
 
 
-def norm(x):
+def norm(x, ord=None, axis=None):
     """
-    Returns the L2-norm of a vector x.
+    Matrix or vector norm.
+
+    See syntax here: https://numpy.org/doc/stable/reference/generated/numpy.linalg.norm.html
     """
     if not is_casadi_type(x):
-        return _onp.linalg.norm(x)
+        return _onp.linalg.norm(x, ord=ord, axis=axis)
 
     else:
-        return _cas.norm_2(x)
+        is_vector = (
+                x.shape[0] == 1 or
+                x.shape[1] == 1
+        )
+
+        if ord is None:
+            if is_vector:
+                ord = 2
+            else:
+                ord = 'fro'
+
+        if ord == 1:
+            return _cas.norm_1(x)
+        elif ord == 2:
+            return _cas.norm_2(x)
+        elif ord == 'fro':
+            return _cas.norm_fro(x)
+        elif ord == _onp.Inf:
+            return _cas.norm_inf()
+        else:
+            try:
+                return sum(abs(x) ** ord) ** (1 / ord)
+            except Exception as e:
+                print(e)
+                raise ValueError("Couldn't interpret `ord` sensibly! Tried to interpret it as a floating-point order "
+                                 "as a last-ditch effort, but that didn't work.")
