16771b317fa636b87418987fef85017560d7281e
Peter Sharpe <peterdsharpe@gmail.com>
2021-03-09 10:43:55 -0500

    deprecate cumsum intil it can be properly implemented

--- aerosandbox/numpy/arithmetic.py
+++ aerosandbox/numpy/arithmetic.py
@@ -54,17 +54,17 @@ def abs(x):
 
 # TODO trace()
 
-def cumsum(x, axis: int = None):
-    """
-    Return the cumulative sum of the elements along a given axis.
-
-    See syntax here: https://numpy.org/doc/stable/reference/generated/numpy.cumsum.html
-    """
-
-    if not is_casadi_type(x):
-        return _onp.cumsum(x, axis=axis)
-
-    else:
-        raise NotImplementedError
-        if axis is None:
-            return _cas.cumsum(_onp.flatten(x))
+# def cumsum(x, axis: int = None):
+#     """
+#     Return the cumulative sum of the elements along a given axis.
+#
+#     See syntax here: https://numpy.org/doc/stable/reference/generated/numpy.cumsum.html
+#     """
+#
+#     if not is_casadi_type(x):
+#         return _onp.cumsum(x, axis=axis)
+#
+#     else:
+#         raise NotImplementedError
+#         if axis is None:
+#             return _cas.cumsum(_onp.flatten(x))
