d79a8e2a48d6e86c22eb9bfcaa8b4f9851f1978b
Antonios <antoniosgeme@gmail.com>
2021-03-12 19:17:33 -0500

    Added mass, generalized functions

--- aerosandbox/library/aerodynamics/unsteady.py
+++ aerosandbox/library/aerodynamics/unsteady.py
@@ -8,15 +8,41 @@ def wagners_function(reduced_time: Union[np.ndarray,float]):
     (Jones, R.T. The Unsteady Lift of a Finite Wing; Technical Report NACA TN-682; NACA: Washington, DC, USA, 1939)
     
     Args:
-        reduced_time (float,np.ndarray) : Reduced time, equal to the number of semichords travelled. See function reduced_time
+        reduced_time (float,np.ndarray) : Equal to the number of semichords travelled. See function calculate_reduced_time
     """
-    return (1 - 0.165 * np.exp(-0.0455 * reduced_time) - 
+    wagner = (1 - 0.165 * np.exp(-0.0455 * reduced_time) - 
                 0.335 * np.exp(-0.3 * reduced_time))
+    try:
+        wagner[reduced_time < 0] = 0
+    except:
+        if reduced_time < 0:
+            wagner = 0
+    
+    return wagner
+
+
+def kussners_function(reduced_time: Union[np.ndarray,float]):
+    """ 
+    A commonly used approximation to Kussner's function (Sears and Sparks 1941)
+    
+    Args:
+        reduced_time (float,np.ndarray) : This is equal to the number of semichords travelled. See function calculate_reduced_time
+    """
+    kussner = 1 - 0.5 * np.exp(-0.13 * reduced_time) - 0.5 * np.exp(-reduced_time) 
+    
+    try:
+        kussner[reduced_time < 0 ] = 0 
+    except:
+        if reduced_time < 0:
+            kussner = 0
+
+    return kussner 
+
 
 
 def calculate_wagner_lift_coefficient(
         reduced_time: Union[float,np.ndarray] , 
-        angle_of_attack: float
+        angle_of_attack: float # In degrees
 ):
     """
     Computes the evolution of the lift coefficient in Wagner's problem which can be interpreted as follows
@@ -29,20 +55,24 @@ def calculate_wagner_lift_coefficient(
     
         Args:
         reduced_time (float,np.ndarray) : Reduced time, equal to the number of semichords travelled. See function reduced_time
-        angle_of_attack (float) 
+        angle_of_attack (float) : The angle of attack in DEGREES
 
     """
-    return 2 * np.pi * angle_of_attack * wagners_function(reduced_time)
+    return 2 * np.pi * np.deg2rad(angle_of_attack) * wagners_function(reduced_time)
 
 
 def calculate_kussner_lift_coefficient(
         reduced_time: Union[float,np.ndarray] , 
         gust_strength:float ,
-        velocity: float
+        velocity: float,
+        angle_of_attack : float = 0, # In degrees
+        chord: float = 1
 ):
     """
-    Computes the evolution of the lift coefficient of a flat plate entering a sharp, transverse, top hat gust
+    Computes the evolution of the lift coefficient of a flat plate entering a 
+    sharp, transverse, top hat gust at a constant angle of attack
     Reduced_time = 0 corresponds to the instance the gust is entered
+    
     
     (Leishman, Principles of Helicopter Aerodynamics, S8.10,S8.11)
     
@@ -50,21 +80,18 @@ def calculate_kussner_lift_coefficient(
         reduced_time (float,np.ndarray) : Reduced time, equal to the number of semichords travelled. See function reduced_time
         gust_strength (float) : velocity in m/s of the top hat gust
         velocity (float) : velocity of the thin airfoil entering the gust
-
+        angle_of_attack (float) : The angle of attack, in degrees
     """
-    return 2 * np.pi * np.arctan(gust_strength / velocity) * kussners_function(reduced_time) 
+    angle_of_attack_radians = np.deg2rad(angle_of_attack)
+    offset = chord / 2 * (1 - np.cos(angle_of_attack_radians))
+    return (2 * np.pi * 
+            np.arctan(gust_strength / velocity) * 
+            np.cos(angle_of_attack_radians) *
+            kussners_function(reduced_time - offset))
 
 
 
 
-def kussners_function(reduced_time: Union[np.ndarray,float]):
-    """ 
-    A commonly used approximation to Kussner's function (Sears and Sparks 1941)
-    
-    Args:
-        reduced_time (float,np.ndarray) : Reduced time, equal to the number of semichords travelled. See function reduced_time
-    """
-    return 1 - 0.5 * np.exp(-0.13 * reduced_time) - 0.5 * np.exp(-reduced_time) 
 
 
 def calculate_reduced_time(
@@ -130,10 +157,49 @@ def duhamel_integral_kussner(
     
     return lift_coefficient
 
+def modified_duhamel_integral_kussner(
+        reduced_time: np.ndarray,
+        gust_velocity: np.ndarray,
+        velocity: float,
+        angle_of_attack: float = 0, # In Degrees
+        chord: float = 1 
+): 
+    """
+    Calculates the duhamel superposition integral of Kussner's problem. 
+    Given some arbitrary transverse velocity profile, the lift coefficient 
+    as a function of reduced time of a flat plate can be computed using this function
+    
+    
+    Args:
+        reduced_time (float,np.ndarray) : Reduced time, equal to the number of semichords travelled. See function reduced_time
+        gust_velocity (np.ndarray) : The transverse velocity profile that the flate plate experiences
+        velocity (float) :The velocity by which the flat plate enters the gust
+        
+    Returns:
+        lift_coefficient (np.ndarray) : The lift coefficient history of the flat plate 
+    """
+    assert np.size(reduced_time) == np.size(gust_velocity),  "The velocity history and time must have the same length"
+    
+    offset = chord / 2 * (1 - np.cos(np.deg2rad(angle_of_attack)))
+    kussner = kussners_function(reduced_time)
+    dK_ds = np.gradient(kussner,reduced_time)
+    lift_coefficient = np.zeros_like(reduced_time)
+    
+    ds =  np.gradient(reduced_time)
+    
+    for i in range(len(reduced_time)):
+        integral_term = 0
+        for j in range(i):
+            integral_term += dK_ds[j]  * gust_velocity[i-j] * ds[j]
+        lift_coefficient[i] = 2 * np.pi / velocity * (gust_velocity[i] * kussner[0] + integral_term)
+    
+    return lift_coefficient
+
+
 
 def duhamel_integral_wagner(
         reduced_time: np.ndarray,
-        angle_of_attack: np.ndarray
+        angle_of_attack: np.ndarray # In degrees
 ): 
     """
     Calculates the duhamel superposition integral of Wagner's problem. 
@@ -149,6 +215,7 @@ def duhamel_integral_wagner(
     """
     assert np.size(reduced_time) == np.size(angle_of_attack),  "The pitching history and time must have the same length"
     
+    angle_of_attack = np.deg2rad(angle_of_attack)
     da_ds = np.gradient(angle_of_attack,reduced_time)
     lift_coefficient = np.zeros_like(reduced_time)
     wagner = wagners_function(reduced_time)
@@ -162,7 +229,34 @@ def duhamel_integral_wagner(
 
 
 
-def added_mass_from_pitching():
+def non_circulatory_lift_from_pitching(
+        reduced_time: np.ndarray,
+        angle_of_attack: np.ndarray # In degrees
+):
+    
+    """
+    This function calculate the lift coefficient due to the added mass of a wing
+    pitching about its midchord moving at constant velocity. 
+    
+    Args:
+        reduced_time (np.ndarray) : Reduced time, equal to the number of semichords travelled. See function reduced_time
+        angle_of_attack (np.ndarray) : The angle of attack as a function of reduced time of the flat plate
+    Returns:
+        lift_coefficient (np.ndarray) : The lift coefficient history of the flat plate 
+    """
+
+    angle_of_attack = np.deg2rad(angle_of_attack)
+    da_ds = np.gradient(angle_of_attack,reduced_time)
+    # TODO: generalize to all unsteady motion
+    
+    return np.pi / 2 * np.cos(angle_of_attack)**2 * da_ds
+    
+    
+
+def pitching_through_transverse_gust():
+    """
+    This function calculates the lift coefficient history of a wing 
+    """
     pass
 
 
@@ -190,12 +284,13 @@ if __name__ == "__main__":
     angle_of_attack[n1:n2] = np.deg2rad(-20)
     
     cl_k = duhamel_integral_kussner(reduced_time,gust_velocity,velocity)
+    cl_k2 = modified_duhamel_integral_kussner(reduced_time,gust_velocity,velocity)
+
     cl_w = duhamel_integral_wagner(reduced_time,angle_of_attack)
         
     plt.figure(dpi=300)
-    plt.plot(reduced_time,cl_w,label="wagner")
     plt.plot(reduced_time,cl_k,label="kussner")
-    plt.plot(reduced_time,cl_k + cl_w,label="total")
+    plt.plot(reduced_time,cl_k2,label="kussner",ls="--")
     plt.xlabel("Reduced time, t*")
     plt.ylabel("$C_\ell$")
     plt.legend()
