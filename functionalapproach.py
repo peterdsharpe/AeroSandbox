a3a2248bd458375ddd219c60aec4da60939605f5
Katherine Zhao <Sylvershadowz@dhcp-10-29-231-219.dyn.mit.edu>
2021-04-28 21:47:36 -0400

    new functional approach

--- functionalapproach.py
+++ functionalapproach.py
@@ -17,24 +17,34 @@ class Node():
     def evaluate(self, x):
         if self.inputs==None:
             return x
-        return self.oper(self.inputs[0].evaluate(x), self.inputs[1].evaluate(x))
+        if input_size[self.oper]==1:
+            return self.oper(self.inputs.evaluate(x))
+        else:
+            return self.oper(self.inputs[0].evaluate(x), self.inputs[1].evaluate(x))
+        
     
     def __str__(self):
         if self.inputs==None:
-            return "Operation: "+str(self.oper)+". No inputs"
+            return "No inputs"
+        if input_size[self.oper]==1:
+            return 'Operation: '+str(self.oper)+ '. Inputs: ' +str(self.inputs)
         return 'Operation: '+str(self.oper)+ '. Inputs: (' +str(self.inputs[0])+', '+str(self.inputs[1])+')'
         
 opti = asb.Opti()   
 def generate_trees(opers, size):
-    yield opti.variable(init_guess=0)
     if size==0:
-        return
+        yield Node(None, None)
     for i in opers:
-        inputs=[]
-        for j in i.input_size:
-            for k in generate_trees(opers, (size-1)//i.input_size):
-                inputs.append(k)
-        yield Node(i, inputs)
+        if input_size[i]==1:
+            for k in generate_trees(opers, (size-1)):
+                yield Node(i, k)
+        if input_size[i]==2:
+            for j in range((size-1)//2):
+                for k in generate_trees(opers, j):
+                    for l in generate_trees(opers, (size-1-j)):
+                        yield Node(i, (k,l))
+                        
+            
 
     
 if __name__ == '__main__':
