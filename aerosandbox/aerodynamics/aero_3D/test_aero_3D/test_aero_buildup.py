e46f4896b5d4096efa85ed83a9d7eb13fe0b4ed7
Peter Sharpe <pds@mit.edu>
2021-08-08 11:01:19 -0700

    merge aero_buildup and avl syntaxes

--- aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_aero_buildup.py
+++ aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_aero_buildup.py
@@ -1,15 +1,13 @@
 import aerosandbox as asb
+import pytest
 from aerosandbox.aerodynamics.aero_3D.test_aero_3D.geometries.conventional import airplane
 
-analysis = asb.AeroBuildup(
-    airplane=airplane,
-    op_point=asb.OperatingPoint(
-        atmosphere=asb.Atmosphere(altitude=0),
-        velocity=10,  # m/s
-        alpha=5,  # In degrees
-        beta=0,  # In degrees
-        p=0,  # About the body x-axis, in rad/sec
-        q=0,  # About the body y-axis, in rad/sec
-        r=0,  # About the body z-axis, in rad/sec
-    ),
-)
+def test_aero_buildup():
+    analysis = asb.AeroBuildup(
+        airplane=airplane,
+        op_point=asb.OperatingPoint(),
+    )
+    return analysis.run()
+
+if __name__ == '__main__':
+    test_aero_buildup()
--- aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_avl.py
+++ aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_avl.py
@@ -1,15 +1,14 @@
 import aerosandbox as asb
-import aerosandbox.numpy as np
 import pytest
 from aerosandbox.aerodynamics.aero_3D.test_aero_3D.geometries.conventional import airplane
 
 
 def test_avl():
-    avl = asb.AVL(
+    analysis = asb.AVL(
         airplane=airplane,
         op_point=asb.OperatingPoint()
     )
-    return avl.run()
+    return analysis.run()
 
 
 if __name__ == '__main__':
