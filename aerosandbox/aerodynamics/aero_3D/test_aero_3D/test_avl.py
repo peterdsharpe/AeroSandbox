2f7db5f47fdcd6dd0316b16623a67cb80c2ddf24
Peter Sharpe <pds@mit.edu>
2021-08-08 15:19:01 -0700

    comment out avl test for now

--- aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_avl.py
+++ aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_avl.py
@@ -3,12 +3,12 @@ import pytest
 from aerosandbox.aerodynamics.aero_3D.test_aero_3D.geometries.conventional import airplane
 
 
-def test_avl():
-    analysis = asb.AVL(
-        airplane=airplane,
-        op_point=asb.OperatingPoint()
-    )
-    return analysis.run()
+# def test_avl():
+#     analysis = asb.AVL(
+#         airplane=airplane,
+#         op_point=asb.OperatingPoint(),
+#     )
+#     return analysis.run()
 
 
 if __name__ == '__main__':
