f2c12a8475f58c2e00ef22d443a1481c24cafc60
Peter Sharpe <pds@mit.edu>
2021-08-10 07:40:35 -0700

    update default resolution

--- aerosandbox/aerodynamics/aero_3D/vortex_lattice_method.py
+++ aerosandbox/aerodynamics/aero_3D/vortex_lattice_method.py
@@ -459,8 +459,8 @@ if __name__ == '__main__':
             q=0,
             r=0,
         ),
-        spanwise_resolution=1,
-        chordwise_resolution=1,
+        spanwise_resolution=12,
+        chordwise_resolution=12,
     )
 
     res = analysis.run()
