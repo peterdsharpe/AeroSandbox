40050700f546992bce9b780c79d07eb5c2de8b9b
Peter Sharpe <pds@mit.edu>
2021-08-10 08:31:52 -0700

    standardize 3D aero test names

--- aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_avl.py
+++ aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_avl.py
@@ -1,15 +1,15 @@
 import aerosandbox as asb
 import pytest
-from aerosandbox.aerodynamics.aero_3D.test_aero_3D.geometries.conventional import airplane
 
-# def test_avl():
-#     analysis = asb.AVL(
-#         airplane=airplane,
-#         op_point=asb.OperatingPoint(),
-#     )
-#     return analysis.run()
+def test_conventional():
+    from aerosandbox.aerodynamics.aero_3D.test_aero_3D.geometries.conventional import airplane
+    analysis = asb.AVL(
+        airplane=airplane,
+        op_point=asb.OperatingPoint(alpha=10),
+    )
+    return analysis.run()
 
 
 if __name__ == '__main__':
-    test_avl()
+    test_conventional()
     # pytest.main()
--- aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_vortex_lattice_method.py
+++ aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_vortex_lattice_method.py
@@ -1,14 +1,14 @@
 import aerosandbox as asb
 import pytest
-from aerosandbox.aerodynamics.aero_3D.test_aero_3D.geometries.conventional import airplane
 
-# def test_vortex_lattice_method():
-#     analysis = asb.VortexLatticeMethod(
-#         airplane=airplane,
-#         op_point=asb.OperatingPoint(),
-#     )
-#     return analysis.run()
+def test_conventional():
+    from aerosandbox.aerodynamics.aero_3D.test_aero_3D.geometries.conventional import airplane
+    analysis = asb.VortexLatticeMethod(
+        airplane=airplane,
+        op_point=asb.OperatingPoint(alpha=10),
+    )
+    return analysis.run()
 
 if __name__ == '__main__':
-    test_vortex_lattice_method()
+    test_conventional()
     # pytest.main()
