fd98604718241b8955f3cfc34687b41360418040
Peter Sharpe <pds@mit.edu>
2021-08-24 11:39:53 -0400

    add bypass for AVL if not present

--- aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_avl.py
+++ aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_avl.py
@@ -1,7 +1,22 @@
 import aerosandbox as asb
 import pytest
 
+
+def is_tool(name):
+    """Check whether `name` is on PATH and marked as executable."""
+
+    # from whichcraft import which
+    from shutil import which
+
+    return which(name) is not None
+
+
+avl_present = is_tool('avl')
+
+
 def test_conventional():
+    if not avl_present:
+        return
     from aerosandbox.aerodynamics.aero_3D.test_aero_3D.geometries.conventional import airplane
     analysis = asb.AVL(
         airplane=airplane,
@@ -9,7 +24,10 @@ def test_conventional():
     )
     return analysis.run()
 
+
 def test_vanilla():
+    if not avl_present:
+        return
     from aerosandbox.aerodynamics.aero_3D.test_aero_3D.geometries.vanilla import airplane
     analysis = asb.AVL(
         airplane=airplane,
@@ -17,13 +35,17 @@ def test_vanilla():
     )
     return analysis.run()
 
+
 def test_flat_plate():
+    if not avl_present:
+        return
     from aerosandbox.aerodynamics.aero_3D.test_aero_3D.geometries.flat_plate import airplane
     analysis = asb.AVL(
         airplane=airplane,
         op_point=asb.OperatingPoint(alpha=10),
     )
     return analysis.run()
+
 
 if __name__ == '__main__':
     # test_conventional()
