f17a17caa19969d8dc95aba18602738bbe4d5664
Peter Sharpe <pds@mit.edu>
2021-09-23 18:08:54 -0400

    add tests for axes conversion

---
+++ aerosandbox/performance/test_performance/test_operating_point_convert_axes.py
@@ -0,0 +1,55 @@
+import aerosandbox as asb
+import aerosandbox.numpy as np
+from typing import List
+import copy
+import pytest
+
+vector = [1, 0, 0]
+op_point = asb.OperatingPoint(alpha=10, beta=5)
+
+
+def chain_conversion(
+        axes: List[str] = ["geometry", "body", "geometry"]
+):
+    x, y, z = copy.deepcopy(vector)
+    for from_axes, to_axes in zip(axes, axes[1:]):
+        x, y, z = op_point.convert_axes(
+            x_from=x,
+            y_from=y,
+            z_from=z,
+            from_axes=from_axes,
+            to_axes=to_axes
+        )
+    return [x, y, z] == pytest.approx(vector)
+
+
+def test_basic():
+    assert chain_conversion()
+
+
+def test_geometry():
+    assert chain_conversion(["body", "geometry", "body"])
+
+
+def test_stability():
+    assert chain_conversion(["body", "stability", "body"])
+
+
+def test_wind():
+    assert chain_conversion(["body", "wind", "body"])
+
+
+def test_cycle():
+    assert chain_conversion([
+        "body",
+        "geometry",
+        "stability",
+        "wind",
+        "body"
+    ])
+
+
+if __name__ == '__main__':
+    print(chain_conversion(["body", "wind"]))
+    pytest.main()
+    chain_conversion()
