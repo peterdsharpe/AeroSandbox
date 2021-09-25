b4b055f82bc5f8a13663a5b33aecdffc27ffc73e
Peter Sharpe <pds@mit.edu>
2021-09-25 13:09:25 -0400

    Added more OperatingPoint.convert_axes() tests

--- aerosandbox/performance/test_performance/test_operating_point_convert_axes_specific_rotations.py
+++ aerosandbox/performance/test_performance/test_operating_point_convert_axes_specific_rotations.py
@@ -47,6 +47,20 @@ def test_beta_wind_body():
     assert y == pytest.approx(0)
     assert z == pytest.approx(0)
 
+def test_alpha_stability_body():
+    op_point = asb.OperatingPoint(
+        alpha=90,
+        beta=0
+    )
+    x, y, z = op_point.convert_axes(
+        0, 0, 1,
+        "body",
+        "stability"
+    )
+    assert x == pytest.approx(1)
+    assert y == pytest.approx(0)
+    assert z == pytest.approx(0)
+
 def test_beta_stability_body():
     op_point = asb.OperatingPoint(
         alpha=0,
@@ -59,6 +73,20 @@ def test_beta_stability_body():
     )
     assert x == pytest.approx(0)
     assert y == pytest.approx(1)
+    assert z == pytest.approx(0)
+
+def test_order_wind_body():
+    op_point = asb.OperatingPoint(
+        alpha=90,
+        beta=90,
+    )
+    x, y, z = op_point.convert_axes(
+        0, 1, 0,
+        "body",
+        "wind"
+    )
+    assert x == pytest.approx(1)
+    assert y == pytest.approx(0)
     assert z == pytest.approx(0)
 
 
