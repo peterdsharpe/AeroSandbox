798b4efaac428b05ab48d362980f2740e8a4df25
Peter Sharpe <pds@mit.edu>
2021-09-25 17:44:50 -0400

    remove unused function

--- aerosandbox/tools/pretty_plots.py
+++ aerosandbox/tools/pretty_plots.py
@@ -260,24 +260,3 @@ def contour(
         plt.gca().clabel(cont, **linelabels_kwargs)
 
     return cont, contf, cbar
-
-# def contour(
-#         func: Callable,
-#         x_range: Tuple[Union[float,int],Union[float,int]],
-#         y_range: Tuple[Union[float,int],Union[float,int]],
-#         resolution:int =50,
-#         show:bool=True,  # type: bool
-# ):
-#     """
-#     Makes a contour plot of a function of 2 variables. Can also plot a list of functions.
-#     :param func: function of form f(x,y) to plot.
-#     :param x_range: Range of x values to plot, expressed as a tuple (x_min, x_max)
-#     :param y_range: Range of y values to plot, expressed as a tuple (y_min, y_max)
-#     :param resolution: Resolution in x and y to plot. [int]
-#     :param show: Should we show the plot?
-#     :return:
-#     """
-#     fig, ax = plt.subplots(1, 1, figsize=(6.4, 4.8), dpi=200)
-#     x = np.linspace(x_range[0], x_range[1], resolution)
-#     y = np.linspace(y_range[0], y_range[1], resolution)
-#     # TODO finish function
