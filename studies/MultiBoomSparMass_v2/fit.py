98b62cd919b971c02d9eaf7877ade1c8a50e0208
Peter Sharpe <pds@mit.edu>
2021-09-29 12:02:07 -0400

    update to meet new syntax

--- studies/MultiBoomSparMass_v2/fit.py
+++ studies/MultiBoomSparMass_v2/fit.py
@@ -1,4 +1,5 @@
-from aerosandbox.modeling.fitting import *
+import aerosandbox as asb
+import aerosandbox.numpy as np
 
 masses = np.load("masses.npy")
 spans = np.load("spans.npy")
@@ -11,14 +12,14 @@ def spar_mass_model(
     return p["c"] * (x["span"] / 40) ** p["span_exp"] * (x["mass"] / 300) ** p["mass_exp"]
 
 
-params = fit(
+fit = asb.FittedModel(
     model=spar_mass_model,
     x_data={
         "mass": masses.reshape(-1),
         "span": spans.reshape(-1),
     },
     y_data=spar_masses.reshape(-1),
-    param_guesses={
+    parameter_guesses={
         "c"       : 1,
         "span_exp": 1,
         "mass_exp": 1,
@@ -26,22 +27,10 @@ params = fit(
     put_residuals_in_logspace=True
 )
 print("\nMODEL\n-----")
-for k, v in params.items():
+for k, v in fit.parameters.items():
     print(
         "%s = %.16f" % (k, v)
     )
 print(
     "spar_mass = c * (span/40) ** span_exp * (mass_eff/300) ** mass_exp"
 )
-
-
-def spar_mass_model_solved(
-        span, mass,
-):
-    return spar_mass_model(
-        x={
-            "mass": mass,
-            "span": span,
-        },
-        p=params
-    )
