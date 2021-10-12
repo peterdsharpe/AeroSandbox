7d84bf995d4be6454f1312cc15a7e9e08a83f93a
Peter Sharpe <pds@mit.edu>
2021-10-12 11:13:35 -0400

    Add interpolation_unstructured.py

---
+++ aerosandbox/modeling/interpolation_unstructured.py
@@ -0,0 +1,8 @@
+from aerosandbox.modeling.interpolation import InterpolatedModel
+
+class UnstructuredInterpolatedModel():
+    """
+    A model that is interpolated to unstructured (i.e., point cloud) N-dimensional data
+
+    """
+    pass
