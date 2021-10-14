9338ca9c44dc1700f958c7fd09a7711c46e81a6b
Peter Sharpe <pds@mit.edu>
2021-10-14 11:10:46 -0400

    reformat

--- aerosandbox/geometry/airfoil/test_airfoil/test_airfoil_polar_generation.py
+++ aerosandbox/geometry/airfoil/test_airfoil/test_airfoil_polar_generation.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
 
     ##### Plot alpha-Re contours
     from aerosandbox.tools.pretty_plots import plt, show_plot, contour
+
     fig, ax = plt.subplots()
     contour(Alpha, Re, CL, levels=30)
     plt.scatter(af.xfoil_data["alpha"], af.xfoil_data["Re"], color="k", alpha=0.2)
@@ -22,8 +23,8 @@ if __name__ == '__main__':
     show_plot()
 
     ##### Plot Polars
-    fig, ax = plt.subplots(2, 2, figsize=(8,8))
-    Re=1e6
+    fig, ax = plt.subplots(2, 2, figsize=(8, 8))
+    Re = 1e6
     alpha_lowres = np.linspace(-15, 15, 31)
     ma = alpha
     mCL = af.CL_function(alpha, Re)
@@ -33,11 +34,11 @@ if __name__ == '__main__':
     xCL = xf_run["CL"]
     xCD = xf_run["CD"]
 
-    plt.sca(ax[0,0])
+    plt.sca(ax[0, 0])
     plt.plot(ma, mCL)
     plt.plot(xa, xCL, ".")
 
-    plt.sca(ax[0,1])
+    plt.sca(ax[0, 1])
     plt.plot(ma, mCD)
     plt.plot(xa, xCD, ".")
     plt.ylim(0, 0.05)
@@ -47,9 +48,9 @@ if __name__ == '__main__':
     plt.plot(xCD, xCL, ".")
     plt.xlim(0, 0.05)
 
-    plt.sca(ax[1,1])
-    plt.plot(ma, mCL/mCD)
-    plt.plot(xa, xCL/xCD, ".")
+    plt.sca(ax[1, 1])
+    plt.plot(ma, mCL / mCD)
+    plt.plot(xa, xCL / xCD, ".")
 
     show_plot()
 
