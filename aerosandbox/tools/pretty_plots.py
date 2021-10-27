adbd30b912902fffc6d1b27ed2f9a659a83bc213
Peter Sharpe <pds@mit.edu>
2021-10-27 15:16:51 -0400

    Disable pretty grids setting for 3D axes, since ax.grid() does not yet work for 3D axes

--- aerosandbox/tools/pretty_plots.py
+++ aerosandbox/tools/pretty_plots.py
@@ -113,8 +113,9 @@ def show_plot(
     if pretty_grids:
         for ax in axes:
             if not ax.get_label() == '<colorbar>':
-                ax.grid(True, 'major', axis='both', linewidth=1.6)
-                ax.grid(True, 'minor', axis='both', linewidth=0.7)
+                if not ax.name == '3d':
+                    ax.grid(True, 'major', axis='both', linewidth=1.6)
+                    ax.grid(True, 'minor', axis='both', linewidth=0.7)
 
     ### Determine if a legend should be shown
     if legend is None:
