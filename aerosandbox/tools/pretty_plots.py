0cc1fd9e64695164d224fafecb57163dc2912e63
Peter Sharpe <pds@mit.edu>
2021-11-01 12:10:36 -0400

    add hline(), vline() to pretty_plots.py

--- aerosandbox/tools/pretty_plots.py
+++ aerosandbox/tools/pretty_plots.py
@@ -144,6 +144,61 @@ def show_plot(
         plt.show()
 
 
+def hline(
+        y,
+        linestyle="--",
+        color="k",
+        text: str = None,
+        text_xloc=0.5,
+        text_ha="center",
+        text_va="bottom",
+        text_kwargs=None,
+):
+    if text_kwargs is None:
+        text_kwargs = {}
+    ax = plt.gca()
+    xlim = ax.get_xlim()
+    plt.axhline(y=y, ls=linestyle, color=color)
+    if text is not None:
+        plt.text(
+            x=text_xloc * xlim[1] + (1 - text_xloc) * xlim[0],
+            y=y,
+            s=text,
+            color=color,
+            horizontalalignment=text_ha,
+            verticalalignment=text_va,
+            **text_kwargs
+        )
+
+
+def vline(
+        x,
+        linestyle="--",
+        color="k",
+        text: str = None,
+        text_yloc=0.5,
+        text_ha="right",
+        text_va="center",
+        text_kwargs=None,
+):
+    if text_kwargs is None:
+        text_kwargs = {}
+    ax = plt.gca()
+    ylim = ax.get_ylim()
+    plt.axvline(x=x, ls=linestyle, color=color)
+    if text is not None:
+        plt.text(
+            x=x,
+            y=text_yloc * ylim[1] + (1 - text_yloc) * ylim[0],
+            s=text,
+            color=color,
+            horizontalalignment=text_ha,
+            verticalalignment=text_va,
+            rotation=90,
+            **text_kwargs
+        )
+
+
 def contour(
         X: np.ndarray,
         Y: np.ndarray,
@@ -241,7 +296,7 @@ def contour(
                 "Warning: All values of the `Z` input to `contour()` should be nonnegative if `z_log_scale` is True!",
                 stacklevel=2
             )
-            Z = np.maximum(Z, 1e-300) # Make all values nonnegative
+            Z = np.maximum(Z, 1e-300)  # Make all values nonnegative
 
     if colorbar_label is not None:
         colorbar_kwargs["label"] = colorbar_label
