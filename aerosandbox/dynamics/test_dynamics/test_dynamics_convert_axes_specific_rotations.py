074ca6c4b5fc3aa826a9ca0c0756581f56ea15a7
Peter Sharpe <pds@mit.edu>
2021-11-02 11:17:46 -0400

    Add dynamics testing for specific axis rotations

---
+++ aerosandbox/dynamics/test_dynamics/test_dynamics_convert_axes_specific_rotations.py
@@ -0,0 +1,108 @@
+import aerosandbox as asb
+import aerosandbox.numpy as np
+from aerosandbox.dynamics.dynamics import FreeBodyDynamics
+from typing import List
+import copy
+import pytest
+
+
+def test_alpha_wind():
+    dyn = FreeBodyDynamics(
+        u=0,
+        w=1,
+        # alpha=90,
+        # beta=0
+    )
+    x, y, z = dyn.convert_axes(
+        0, 0, 1,
+        "geometry",
+        "wind"
+    )
+    assert x == pytest.approx(-1)
+    assert y == pytest.approx(0)
+    assert z == pytest.approx(0)
+
+def test_beta_wind():
+    dyn = FreeBodyDynamics(
+        u=0,
+        v=1,
+        # alpha=0,
+        # beta=90
+    )
+    x, y, z = dyn.convert_axes(
+        0, 1, 0,
+        "geometry",
+        "wind"
+    )
+    assert x == pytest.approx(1)
+    assert y == pytest.approx(0)
+    assert z == pytest.approx(0)
+
+def test_beta_wind_body():
+    dyn = FreeBodyDynamics(
+        u=0,
+        v=1,
+        # alpha=0,
+        # beta=90
+    )
+    x, y, z = dyn.convert_axes(
+        0, 1, 0,
+        "body",
+        "wind"
+    )
+    assert x == pytest.approx(1)
+    assert y == pytest.approx(0)
+    assert z == pytest.approx(0)
+
+def test_alpha_stability_body():
+    dyn = FreeBodyDynamics(
+        u=0,
+        w=1,
+        # alpha=90,
+        # beta=0
+    )
+    x, y, z = dyn.convert_axes(
+        0, 0, 1,
+        "body",
+        "stability"
+    )
+    assert x == pytest.approx(1)
+    assert y == pytest.approx(0)
+    assert z == pytest.approx(0)
+
+def test_beta_stability_body():
+    dyn = FreeBodyDynamics(
+        u=0,
+        v=1
+        # alpha=0,
+        # beta=90
+    )
+    x, y, z = dyn.convert_axes(
+        0, 1, 0,
+        "body",
+        "stability"
+    )
+    assert x == pytest.approx(0)
+    assert y == pytest.approx(1)
+    assert z == pytest.approx(0)
+
+def test_order_wind_body():
+    dyn = FreeBodyDynamics(
+        u=0,
+        v=1,
+        phi=np.pi/2,
+        # alpha=90,
+        # beta=90,
+    )
+    x, y, z = dyn.convert_axes(
+        0, 1, 0,
+        "body",
+        "wind"
+    )
+    assert x == pytest.approx(1)
+    assert y == pytest.approx(0)
+    assert z == pytest.approx(0)
+
+
+if __name__ == '__main__':
+    pytest.main()
