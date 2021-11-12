1dd64c4e9176b7c1cee9809b9daf14e30872e2a8
Peter Sharpe <pds@mit.edu>
2021-11-12 13:42:41 -0500

    remove relative imports

--- aerosandbox/modeling/__init__.py
+++ aerosandbox/modeling/__init__.py
@@ -1,3 +1,3 @@
-from .fitting import FittedModel
-from .interpolation import InterpolatedModel
-from .interpolation_unstructured import UnstructuredInterpolatedModel
+from aerosandbox.modeling.fitting import FittedModel
+from aerosandbox.modeling.interpolation import InterpolatedModel
+from aerosandbox.modeling.interpolation_unstructured import UnstructuredInterpolatedModel
