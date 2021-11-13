42abac3f511260991a1ad9c038b1e405187b64af
Peter Sharpe <pds@mit.edu>
2021-11-13 16:38:15 -0500

    add test assertions

--- aerosandbox/dynamics/point_mass/point_2D/test/test_cannonball.py
+++ aerosandbox/dynamics/point_mass/point_2D/test/test_cannonball.py
@@ -1,5 +1,6 @@
 import aerosandbox as asb
 import aerosandbox.numpy as np
+import pytest
 from scipy import integrate
 
 t_eval = np.linspace(0, 10, 1001)
@@ -9,8 +10,8 @@ speed_0 = (u_e_0 ** 2 + w_e_0 ** 2) ** 0.5
 gamma_0 = np.arctan2(-w_e_0, u_e_0)
 ivp_kwargs = {
     "vectorized": True,
-    "rtol": 1e-12,
-    "atol": 1e-14,
+    "rtol": 1e-9,
+    "atol": 1e-9,
 }
 
 def test_trajectory_Cartesian_with_drag(plot=False):
@@ -137,6 +138,12 @@ def final_position_Cartesian(drag=False):
         dyn_init.mass_props,
         **{k: v for k, v in zip(dyn_init.state.keys(), res.y)}
     )
+    if drag:
+        assert dyn[-1].x_e == pytest.approx(198.53465, abs=1e-2)
+        assert dyn[-1].z_e == pytest.approx(44.452918, abs=1e-2)
+    else:
+        assert dyn[-1].x_e == pytest.approx(1000, abs=1e-2)
+        assert dyn[-1].z_e == pytest.approx(-509.5, abs=1e-2)
     return dyn[-1]
 
 def final_position_SpeedGamma(drag=False):
@@ -174,12 +181,28 @@ def final_position_SpeedGamma(drag=False):
         dyn_init.mass_props,
         **{k: v for k, v in zip(dyn_init.state.keys(), res.y)}
     )
+    if drag:
+        assert dyn[-1].x_e == pytest.approx(198.53465, abs=1e-2)
+        assert dyn[-1].z_e == pytest.approx(44.452918, abs=1e-2)
+    else:
+        assert dyn[-1].x_e == pytest.approx(1000, abs=1e-2)
+        assert dyn[-1].z_e == pytest.approx(-509.5, abs=1e-2)
     return dyn[-1]
+
+def test_final_position_Cartesian():
+    final_position_Cartesian(drag=True)
+    final_position_Cartesian(drag=False)
+
+def test_final_position_SpeedGamma():
+    final_position_SpeedGamma(drag=True)
+    final_position_SpeedGamma(drag=False)
 
 
 if __name__ == '__main__':
     test_trajectory_Cartesian_with_drag(False)
     test_trajectory_SpeedGamma_with_drag(False)
+    test_final_position_Cartesian()
+    test_final_position_SpeedGamma()
     print(final_position_Cartesian(True))
     print(final_position_SpeedGamma(True))
     print(final_position_Cartesian(False))
