b55c69f6c3670ae50bfabf0346c72bcc3d60146a
Peter Sharpe <pds@mit.edu>
2021-11-15 21:34:40 -0500

    Rework Cannonball to be more reusable and clear

--- aerosandbox/dynamics/point_mass/point_2D/test/test_cannonball.py
+++ aerosandbox/dynamics/point_mass/point_2D/test/test_cannonball.py
@@ -3,51 +3,61 @@ import aerosandbox.numpy as np
 import pytest
 from scipy import integrate
 
-t_eval = np.linspace(0, 10, 1001)
 u_e_0 = 100
 w_e_0 = -100
 speed_0 = (u_e_0 ** 2 + w_e_0 ** 2) ** 0.5
 gamma_0 = np.arctan2(-w_e_0, u_e_0)
-ivp_kwargs = {
-    "vectorized": True,
-    "rtol": 1e-9,
-    "atol": 1e-9,
-}
 
-def test_trajectory_Cartesian_with_drag(plot=False):
-    dyn_init = asb.DynamicsPointMass2DCartesian(
-        mass_props=asb.MassProperties(mass=1),
-        x_e=0,
-        z_e=0,
-        u_e=u_e_0,
-        w_e=w_e_0,
-    )
+time=np.linspace(0, 10, 1001)
+
+def get_trajectory(
+        parameterization: type = asb.DynamicsPointMass2DCartesian,
+        gravity=True,
+        drag=True,
+        plot=False
+):
+    if parameterization is asb.DynamicsPointMass2DCartesian:
+        dyn = asb.DynamicsPointMass2DCartesian(
+            mass_props=asb.MassProperties(mass=1),
+            x_e=0,
+            z_e=0,
+            u_e=u_e_0,
+            w_e=w_e_0,
+        )
+    elif parameterization is asb.DynamicsPointMass2DSpeedGamma:
+        dyn = asb.DynamicsPointMass2DSpeedGamma(
+            mass_props=asb.MassProperties(mass=1),
+            x_e=0,
+            z_e=0,
+            speed=speed_0,
+            gamma=gamma_0,
+        )
+    else:
+        raise ValueError("Bad value of `parameterization`!")
 
     def derivatives(t, y):
-        this_dyn = dyn_init.__class__(
-            dyn_init.mass_props,
-            **{k: v for k, v in zip(dyn_init.state.keys(), y)}
-        )
-        this_dyn.add_gravity_force()
-        this_dyn.add_force(
-            Fx=-0.01 * this_dyn.speed ** 2,
-            axes="wind"
-        )
+        this_dyn = dyn.get_new_instance_with_state(y)
+        if gravity:
+            this_dyn.add_gravity_force()
+        if drag:
+            this_dyn.add_force(
+                Fx=-0.01 * this_dyn.speed ** 2,
+                axes="wind"
+            )
 
-        return tuple(this_dyn.state_derivatives().values())
+        return this_dyn.unpack_state(this_dyn.state_derivatives())
 
     res = integrate.solve_ivp(
         fun=derivatives,
-        t_span=(t_eval[0], t_eval[-1]),
-        t_eval=t_eval,
-        y0=tuple(dyn_init.state.values()),
-        **ivp_kwargs
+        t_span=(time[0], time[-1]),
+        t_eval=time,
+        y0=dyn.unpack_state(),
+        vectorized=True,
+        rtol=1e-9,
+        atol=1e-9,
     )
 
-    dyn = dyn_init.__class__(
-        dyn_init.mass_props,
-        **{k: v for k, v in zip(dyn_init.state.keys(), res.y)}
-    )
+    dyn = dyn.get_new_instance_with_state(res.y)
 
     if plot:
         import matplotlib.pyplot as plt;
@@ -58,152 +68,45 @@ def test_trajectory_Cartesian_with_drag(plot=False):
         p.equal()
         p.show_plot("Trajectory", "$x_e$", "$z_e$")
 
+    return dyn
 
-def test_trajectory_SpeedGamma_with_drag(plot=False):
-    dyn_init = asb.DynamicsPointMass2DSpeedGamma(
-        mass_props=asb.MassProperties(mass=1),
-        x_e=0,
-        z_e=0,
-        speed=speed_0,
-        gamma=gamma_0,
+
+def test_final_position_Cartesian_with_drag():
+    dyn = get_trajectory(
+        parameterization=asb.DynamicsPointMass2DCartesian,
+        drag=True
     )
+    assert dyn[-1].x_e == pytest.approx(198.53465, abs=1e-2)
+    assert dyn[-1].z_e == pytest.approx(44.452918, abs=1e-2)
 
-    def derivatives(t, y):
-        this_dyn = dyn_init.__class__(
-            dyn_init.mass_props,
-            **{k: v for k, v in zip(dyn_init.state.keys(), y)}
-        )
-        this_dyn.add_gravity_force()
-        this_dyn.add_force(
-            Fx=-0.01 * this_dyn.speed ** 2,
-            axes="wind"
-        )
 
-        return tuple(this_dyn.state_derivatives().values())
-
-    res = integrate.solve_ivp(
-        fun=derivatives,
-        t_span=(t_eval[0], t_eval[-1]),
-        t_eval=t_eval,
-        y0=tuple(dyn_init.state.values()),
-        **ivp_kwargs
+def test_final_position_Cartesian_no_drag():
+    dyn = get_trajectory(
+        parameterization=asb.DynamicsPointMass2DCartesian,
+        drag=False
     )
+    assert dyn[-1].x_e == pytest.approx(1000, abs=1e-2)
+    assert dyn[-1].z_e == pytest.approx(-509.5, abs=1e-2)
 
-    dyn = dyn_init.__class__(
-        dyn_init.mass_props,
-        **{k: v for k, v in zip(dyn_init.state.keys(), res.y)}
+
+def test_final_position_SpeedGamma_with_drag():
+    dyn = get_trajectory(
+        parameterization=asb.DynamicsPointMass2DSpeedGamma,
+        drag=True
     )
+    assert dyn[-1].x_e == pytest.approx(198.53465, abs=1e-2)
+    assert dyn[-1].z_e == pytest.approx(44.452918, abs=1e-2)
 
-    if plot:
-        import matplotlib.pyplot as plt;
-        import aerosandbox.tools.pretty_plots as p
 
-        fig, ax = plt.subplots()
-        p.plot_color_by_value(dyn.x_e, dyn.altitude, c=dyn.speed, colorbar=True)
-        p.equal()
-        p.show_plot("Trajectory", "$x_e$", "$z_e$")
-
-def final_position_Cartesian(drag=False):
-    dyn_init = asb.DynamicsPointMass2DCartesian(
-        mass_props=asb.MassProperties(mass=1),
-        x_e=0,
-        z_e=0,
-        u_e=u_e_0,
-        w_e=w_e_0,
+def test_final_position_SpeedGamma_no_drag():
+    dyn = get_trajectory(
+        parameterization=asb.DynamicsPointMass2DSpeedGamma,
+        drag=False
     )
-
-    def derivatives(t, y):
-        this_dyn = dyn_init.__class__(
-            dyn_init.mass_props,
-            **{k: v for k, v in zip(dyn_init.state.keys(), y)}
-        )
-        this_dyn.add_gravity_force()
-        if drag:
-            this_dyn.add_force(
-                Fx=-0.01 * this_dyn.speed ** 2,
-                axes="wind"
-            )
-
-        return tuple(this_dyn.state_derivatives().values())
-
-    res = integrate.solve_ivp(
-        fun=derivatives,
-        t_span=(t_eval[0], t_eval[-1]),
-        t_eval=t_eval,
-        y0=tuple(dyn_init.state.values()),
-        **ivp_kwargs
-    )
-
-    dyn = dyn_init.__class__(
-        dyn_init.mass_props,
-        **{k: v for k, v in zip(dyn_init.state.keys(), res.y)}
-    )
-    if drag:
-        assert dyn[-1].x_e == pytest.approx(198.53465, abs=1e-2)
-        assert dyn[-1].z_e == pytest.approx(44.452918, abs=1e-2)
-    else:
-        assert dyn[-1].x_e == pytest.approx(1000, abs=1e-2)
-        assert dyn[-1].z_e == pytest.approx(-509.5, abs=1e-2)
-    return dyn[-1]
-
-def final_position_SpeedGamma(drag=False):
-    dyn_init = asb.DynamicsPointMass2DSpeedGamma(
-        mass_props=asb.MassProperties(mass=1),
-        x_e=0,
-        z_e=0,
-        speed=speed_0,
-        gamma=gamma_0,
-    )
-
-    def derivatives(t, y):
-        this_dyn = dyn_init.__class__(
-            dyn_init.mass_props,
-            **{k: v for k, v in zip(dyn_init.state.keys(), y)}
-        )
-        this_dyn.add_gravity_force()
-        if drag:
-            this_dyn.add_force(
-                Fx=-0.01 * this_dyn.speed ** 2,
-                axes="wind"
-            )
-
-        return tuple(this_dyn.state_derivatives().values())
-
-    res = integrate.solve_ivp(
-        fun=derivatives,
-        t_span=(t_eval[0], t_eval[-1]),
-        t_eval=t_eval,
-        y0=tuple(dyn_init.state.values()),
-        **ivp_kwargs
-    )
-
-    dyn = dyn_init.__class__(
-        dyn_init.mass_props,
-        **{k: v for k, v in zip(dyn_init.state.keys(), res.y)}
-    )
-    if drag:
-        assert dyn[-1].x_e == pytest.approx(198.53465, abs=1e-2)
-        assert dyn[-1].z_e == pytest.approx(44.452918, abs=1e-2)
-    else:
-        assert dyn[-1].x_e == pytest.approx(1000, abs=1e-2)
-        assert dyn[-1].z_e == pytest.approx(-509.5, abs=1e-2)
-    return dyn[-1]
-
-def test_final_position_Cartesian():
-    final_position_Cartesian(drag=True)
-    final_position_Cartesian(drag=False)
-
-def test_final_position_SpeedGamma():
-    final_position_SpeedGamma(drag=True)
-    final_position_SpeedGamma(drag=False)
+    assert dyn[-1].x_e == pytest.approx(1000, abs=1e-2)
+    assert dyn[-1].z_e == pytest.approx(-509.5, abs=1e-2)
 
 
+#
 if __name__ == '__main__':
-    test_trajectory_Cartesian_with_drag(False)
-    test_trajectory_SpeedGamma_with_drag(False)
-    test_final_position_Cartesian()
-    test_final_position_SpeedGamma()
-    print(final_position_Cartesian(True))
-    print(final_position_SpeedGamma(True))
-    print(final_position_Cartesian(False))
-    print(final_position_SpeedGamma(False))
+    pytest.main()
