9fe55a12d5e7534def97f2bd3d7886169f9f6ee5
Peter Sharpe <pds@mit.edu>
2021-11-15 20:03:31 -0500

    Remove separate speed, gamma properties

--- aerosandbox/dynamics/point_mass/common_point_mass.py
+++ aerosandbox/dynamics/point_mass/common_point_mass.py
@@ -180,14 +180,6 @@ class _DynamicsPointMassBaseClass(AeroSandboxObject, ABC):
     def altitude(self):
         return -self.z_e
 
-    @abstractproperty
-    def speed(self) -> float:
-        pass
-
-    @abstractproperty
-    def gamma(self) -> float:
-        pass
-
     @property
     def translational_kinetic_energy(self) -> float:
         return 0.5 * self.mass_props.mass * self.speed ** 2
