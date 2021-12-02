284ecf86d269958b0037e28ebbc8b68ac36bf72d
Peter <pds@mit.edu>
2021-12-02 14:33:53 -0500

    add units

--- aerosandbox/tools/units.py
+++ aerosandbox/tools/units.py
@@ -1,1 +1,14 @@
 # TODO
+
+# Lengths (equivalents in meters)
+inch = 0.0254
+foot = 12 * inch
+yard = 3 * foot
+mile = 5280 * foot
+
+# Masses (equivalents in kilograms)
+slug = 14.5939
+lbm = 0.4536
+
+# Force (equivalents in Newtons)
+lbf = 4.448
