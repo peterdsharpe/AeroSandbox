28ddc9f03001fbe1cea1c758fea4720de5671287
Peter Sharpe <pds@mit.edu>
2021-12-02 17:27:47 -0500

    adding more units. Adding more precision

--- aerosandbox/tools/units.py
+++ aerosandbox/tools/units.py
@@ -1,14 +1,22 @@
-# TODO
-
 # Lengths (equivalents in meters)
 inch = 0.0254
 foot = 12 * inch
 yard = 3 * foot
 mile = 5280 * foot
+naut_mile = 1852
 
 # Masses (equivalents in kilograms)
-slug = 14.5939
-lbm = 0.4536
+slug = 14.59390
+lbm = 0.45359237
+short_ton = 2000 * lbm
 
 # Force (equivalents in Newtons)
-lbf = 4.448
+lbf = 4.4482216152605
+
+# Time (equivalents in seconds)
+minute = 60
+hour = 60 * minute
+day = 86400
+week = 7 * day
+year = 365.25 * day
+month = year / 12
