4d17dc612245064c391a32d8bcc2702eb63a9372
Peter Sharpe <pds@mit.edu>
2022-02-11 15:28:52 -0500

    add test for transonic optimization

---
+++ aerosandbox/library/aerodynamics/test_aerodynamics/test_transonic_optimization.py
@@ -0,0 +1,37 @@
+import aerosandbox as asb
+import aerosandbox.numpy as np
+import pytest
+
+from aerosandbox.library.aerodynamics import transonic
+
+
+def test_simple_scalar_optimization():
+    opti = asb.Opti()
+    mach = opti.variable(init_guess=0.8, )
+    CD_induced = 0.1 / mach
+    CD_wave = transonic.approximate_CD_wave(
+        mach=mach,
+        mach_crit=0.7,
+        CD_wave_at_fully_supersonic=1,
+        # only_model_drag_rise=True
+    )
+    CD = CD_induced + CD_wave
+    opti.minimize(CD)
+    sol = opti.solve()
+
+    opti = asb.Opti()
+    mach = opti.variable(init_guess=0.8, )
+    CD_induced = 0.1 / mach
+    CD_wave = transonic.approximate_CD_wave(
+        mach=mach,
+        mach_crit=0.7,
+        CD_wave_at_fully_supersonic=1,
+        only_model_drag_rise=True
+    )
+    CD = CD_induced + CD_wave
+    opti.minimize(CD)
+    sol = opti.solve()
+
+
+if __name__ == '__main__':
+    pytest.main()
