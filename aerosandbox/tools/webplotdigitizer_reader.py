0a8aeff72fc644b4fb00b1bec20810d9dcacdf53
Peter Sharpe <pds@mit.edu>
2022-03-16 09:48:15 -0400

    Modified webplotdigitizer to work with single- and multi-series CSVs.

--- aerosandbox/tools/webplotdigitizer_reader.py
+++ aerosandbox/tools/webplotdigitizer_reader.py
@@ -9,31 +9,63 @@ import numpy as np
 from typing import Dict
 
 
+def string_to_float(s: str) -> float:
+    """Converts a string input to a float. If not possible, returns NaN."""
+    try:
+        return float(s)
+    except ValueError:
+        return np.NaN
+
+
+def remove_nan_rows(a: np.ndarray) -> np.ndarray:
+    """Removes any rows in a 2D ndarray where any of the entries are NaN."""
+    nan_rows = np.any(np.isnan(a), axis=1)
+    return a[~nan_rows, :]
+
+
 def read_webplotdigitizer_csv(filename) -> Dict[str, np.ndarray]:
+    """
+    Reads a CSV file produced by WebPlotDigitizer (https://automeris.io/WebPlotDigitizer/).
+
+    If there's only one data series, produces a Dict with key "data" and value 2D ndarray.
+
+    If there are multiple data series, produces a Dict with keys of the names and values of 2D ndarrays.
+
+    2D ndarrays are sorted by their X-values before being returned.
+
+    Args:
+        filename: Filename, as a string or pathlib Path, or equivalent.
+
+    Returns: A dictionary where keys are series names and values are data points.
+
+    """
     delimiter = ","
     with open(filename, "r") as f:
         lines = f.readlines()
 
-    titles = lines[0].split(delimiter)[::2]
+    has_titles = np.any([
+        np.isnan(string_to_float(s))
+        for s in lines[0].split(delimiter)
+    ])
 
-    def string_to_float(s: str) -> float:
-        try:
-            return float(s)
-        except ValueError:
-            return np.NaN
+    if has_titles:
+        titles = lines[0].split(delimiter)[::2]
+        first_data_row = 2
+    else:
+        titles = ["data"]
+        first_data_row = 0
 
-    data = [
+    all_data = np.array([
         [string_to_float(item) for item in line.split(delimiter)]
-        for line in lines[2:]
-    ]
+        for line in lines[first_data_row:]
+    ], dtype=float)
 
-    data = np.array(data, dtype=float)
+    output = {}
 
-    def remove_nan_rows(a: np.ndarray) -> np.ndarray:
-        nan_rows = np.any(np.isnan(a), axis=1)
-        return a[~nan_rows, :]
+    for i, title in enumerate(titles):
+        series = remove_nan_rows(all_data[:, 2 * i: 2 * i + 2])
+        sort_order = np.argsort(series[:, 0])
 
-    return {
-        title: remove_nan_rows(data[:, 2 * i:2 * i + 2])
-        for i, title in enumerate(titles)
-    }
+        output[title] = series[sort_order, :]
+
+    return output
