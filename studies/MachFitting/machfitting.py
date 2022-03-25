79ea69be71fc8f312a8f784e81c2f08c05f15b0c
Peter Sharpe <pds@mit.edu>
2022-03-25 02:38:08 -0400

    modernize code

--- studies/MachFitting/machfitting.py
+++ studies/MachFitting/machfitting.py
@@ -1,10 +1,7 @@
-from aerosandbox.modeling.fitting import fit
+import aerosandbox as asb
+import aerosandbox.numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-from aerosandbox import cas
-
-sns.set(palette=sns.color_palette("husl"))
-import numpy as np
+import aerosandbox.tools.pretty_plots as p
 
 
 def beta(mach):
@@ -15,11 +12,11 @@ machs_to_fit = np.linspace(0.001, 0.999, 500)
 
 
 def sigmoid(x):
-    return 1 / (1 + cas.exp(x))
+    return 1 / (1 + np.exp(x))
 
 
 def inv_sigmoid(x):
-    return cas.log(1 / x - 1)
+    return np.log(1 / x - 1)
 
 
 weights = np.ones_like(machs_to_fit)
@@ -27,29 +24,6 @@ weights[0] = 1000
 weights[machs_to_fit > 0.9] = 0.5
 weights[machs_to_fit > 0.95] = 0.25
 
-
-# def model(x, p):
-#     return sigmoid(
-#         p["m"]*x+p["th"]*cas.tanh((x-p["o"]))+p["c"]
-#     )
-#
-#
-# fit_params = fit(
-#     model=model,
-#     x_data=machs_to_fit,
-#     y_data=beta(machs_to_fit),
-#     param_guesses={
-#         "m": 10,
-#         "c": -0.5,
-#         "th": -1,
-#         "o": 0.6
-#     },
-#     param_bounds={
-#         "o": (0.25, 0.75),
-#         "th": (None, 0),
-#     },
-#     weights=weights
-# )
 
 def model(x, p):
     return sigmoid(
@@ -59,11 +33,11 @@ def model(x, p):
     )
 
 
-fit_params = fit(
+fit = asb.FittedModel(
     model=model,
     x_data=machs_to_fit,
     y_data=beta(machs_to_fit),
-    param_guesses={
+    parameter_guesses={
         "p1": 4.6,
         "o1": 0.89,
         "p3": 140,
@@ -71,7 +45,7 @@ fit_params = fit(
         "p5": 120,
         "o5": 0.79,
     },
-    param_bounds={
+    parameter_bounds={
         "o3": (0, 1),
         "o5": (0, 1),
     },
@@ -79,33 +53,35 @@ fit_params = fit(
     # residual_norm_type="deviation"
 )
 
-error = model(machs_to_fit, fit_params) - beta(machs_to_fit)
+error = fit(machs_to_fit) - beta(machs_to_fit)
 error = np.array(error)
 
 machs_to_plot = np.linspace(-0.5, 1.5, 500)
 fig, ax = plt.subplots(1, 1, figsize=(8, 7), dpi=200)
-plt.subplot("221")
+plt.subplot(221)
 plt.plot(machs_to_plot, beta(machs_to_plot), ".", label="")
-plt.plot(machs_to_plot, model(machs_to_plot, fit_params))
+plt.plot(machs_to_plot, fit(machs_to_plot))
 plt.ylim(-0.05, 1.05)
 plt.title("Fit: Normal Space")
 plt.xlabel(r"Mach $M$ [-]")
 plt.ylabel(r"$\beta = \sqrt{1-M^2}$")
-plt.subplot("222")
+plt.subplot(222)
 plt.plot(machs_to_plot, inv_sigmoid(beta(machs_to_plot)), ".", label="")
-plt.plot(machs_to_plot, inv_sigmoid(model(machs_to_plot, fit_params)))
+plt.plot(machs_to_plot, inv_sigmoid(fit(machs_to_plot)))
 plt.ylim(-15, 5)
 plt.title("Fit: Inverse Sigmoid Space")
 plt.xlabel(r"Mach $M$ [-]")
 plt.ylabel(r"$\sigma^{-1}\left(\beta\right)$")
-plt.subplot("223")
-plt.plot(machs_to_plot, cas.log(beta(machs_to_plot)), ".", label="")
-plt.plot(machs_to_plot, cas.log(model(machs_to_plot, fit_params)))
+plt.subplot(223)
+plt.plot(machs_to_plot, np.log(beta(machs_to_plot)), ".", label="")
+plt.plot(machs_to_plot, np.log(fit(machs_to_plot)))
 plt.ylim(-2.5, 0.5)
 plt.title("Fit: Log Space")
 plt.xlabel(r"Mach $M$ [-]")
 plt.ylabel(r"$\ln(\beta)$")
-plt.subplot("224")
+plt.subplot(224)
 plt.plot(machs_to_fit, error)
 plt.title("Error in Fit range")
 plt.savefig("machfitting.png")
+
+p.show_plot()
