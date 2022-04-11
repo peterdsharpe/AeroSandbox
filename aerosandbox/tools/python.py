b004228106de563ea5265514bcd5b97b302ef6bc
Peter Sharpe <pds@mit.edu>
2022-04-11 10:11:25 -0400

    add tools for lazy import

--- aerosandbox/tools/python.py
+++ aerosandbox/tools/python.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json, sys, os
+import importlib
 
 
 def convert_ipynb_to_py(
@@ -81,3 +82,26 @@ def run_all_python_files(path: Path, recursive=True, verbose=True) -> None:
             print(f"##### Opening directory: {path}")
         for subpath in path.iterdir():
             run_all_python_files(subpath, recursive=recursive, verbose=verbose)
+
+
+def lazy_import(name):
+    """
+    Allows you to lazily import a module.
+
+    Usage:
+    >>> asb = lazy_import("aerosandbox")  # Runs instantly
+    >>> asb.Airplane()  # When this is called, ASB will be loaded.
+
+    Args:
+        name: The package name
+
+    Returns: The module itself, to be loaded on first use.
+
+    """
+    spec = importlib.util.find_spec(name)
+    loader = importlib.util.LazyLoader(spec.loader)
+    spec.loader = loader
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[name] = module
+    loader.exec_module(module)
+    return module
