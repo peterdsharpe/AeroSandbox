578c6ee49aa86c5c2deda3c34315f0f9db32b1fc
Peter Sharpe <pds@mit.edu>
2022-06-10 17:50:54 -0400

    update avl_validation.py to be more clear on airplane definition

--- aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_aero_buildup/avl_validation.py
+++ aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_aero_buildup/avl_validation.py
@@ -1,7 +1,90 @@
-from aerosandbox.aerodynamics.aero_3D.test_aero_3D.geometries.conventional import airplane
 import aerosandbox as asb
 import aerosandbox.numpy as np
 import pytest
+
+wing_airfoil = asb.Airfoil("naca0010")# asb.Airfoil("sd7037")
+tail_airfoil = asb.Airfoil("naca0010")
+
+### Define the 3D geometry you want to analyze/optimize.
+# Here, all distances are in meters and all angles are in degrees.
+airplane = asb.Airplane(
+    name="Peter's Glider",
+    xyz_ref=[0, 0, 0],  # CG location
+    wings=[
+        asb.Wing(
+            name="Main Wing",
+            symmetric=True,  # Should this wing be mirrored across the XZ plane?
+            xsecs=[  # The wing's cross ("X") sections
+                asb.WingXSec(  # Root
+                    xyz_le=[0, 0, 0],  # Coordinates of the XSec's leading edge, relative to the wing's leading edge.
+                    chord=0.18,
+                    twist=0,  # degrees
+                    airfoil=wing_airfoil,  # Airfoils are blended between a given XSec and the next one.
+                ),
+                asb.WingXSec(  # Mid
+                    xyz_le=[0.01, 0.5, 0],
+                    chord=0.16,
+                    twist=0,
+                    airfoil=wing_airfoil,
+                ),
+                asb.WingXSec(  # Tip
+                    xyz_le=[0.08, 1, 0.1],
+                    chord=0.08,
+                    twist=-0,
+                    airfoil=wing_airfoil,
+                ),
+            ]
+        ),
+        asb.Wing(
+            name="Horizontal Stabilizer",
+            symmetric=True,
+            xsecs=[
+                asb.WingXSec(  # root
+                    xyz_le=[0, 0, 0],
+                    chord=0.1,
+                    twist=-10,
+                    airfoil=tail_airfoil,
+                ),
+                asb.WingXSec(  # tip
+                    xyz_le=[0.02, 0.17, 0],
+                    chord=0.08,
+                    twist=-10,
+                    airfoil=tail_airfoil
+                )
+            ]
+        ).translate([0.6, 0, 0.06]),
+        asb.Wing(
+            name="Vertical Stabilizer",
+            symmetric=False,
+            xsecs=[
+                asb.WingXSec(
+                    xyz_le=[0, 0, 0],
+                    chord=0.1,
+                    twist=0,
+                    airfoil=tail_airfoil,
+                ),
+                asb.WingXSec(
+                    xyz_le=[0.04, 0, 0.15],
+                    chord=0.06,
+                    twist=0,
+                    airfoil=tail_airfoil
+                )
+            ]
+        ).translate([0.6, 0, 0.07])
+    ],
+    fuselages=[
+        asb.Fuselage(
+            name="Fuselage",
+            xsecs=[
+                asb.FuselageXSec(
+                    xyz_c=[xi, 0, 0],
+                    radius=1 * asb.Airfoil("dae51").local_thickness(x_over_c=xi)
+                )
+                for xi in np.cosspace(0, 1, 30)
+            ]
+        )
+    ]
+)
 
 for i, wing in enumerate(airplane.wings):
     for j, xsec in enumerate(wing.xsecs):
@@ -14,14 +97,10 @@ for i, wing in enumerate(airplane.wings):
         )
         airplane.wings[i].xsecs[j].airfoil = af
 
-# wing = airplane.wings[0]
-# wing.xsecs = [wing.xsecs[0], wing.xsecs[1]]
-# airplane.wings = [wing]
-# airplane.fuselages = []
-
 op_point = asb.OperatingPoint(
     velocity=100,
-    alpha=0,
+    alpha=5,
+    beta=0,
 )
 
 ab = asb.AeroBuildup(
