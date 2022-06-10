ea7b8a2b0da3a860a41a83791a621ef5c1d97bbf
Peter Sharpe <pds@mit.edu>
2022-06-09 23:55:03 -0400

    simplify all trig functions by precomputing the conversion factor

--- aerosandbox/numpy/trig.py
+++ aerosandbox/numpy/trig.py
@@ -1,15 +1,18 @@
 import numpy as _onp
 from numpy import pi as _pi
 
+_deg2rad = 180. / _pi
+_rad2deg = _pi / 180.
+
 
 def degrees(x):
     """Converts an input x from radians to degrees"""
-    return x * 180 / _pi
+    return x * _deg2rad
 
 
 def radians(x):
     """Converts an input x from degrees to radians"""
-    return x * _pi / 180
+    return x * _rad2deg
 
 
 def sind(x):
