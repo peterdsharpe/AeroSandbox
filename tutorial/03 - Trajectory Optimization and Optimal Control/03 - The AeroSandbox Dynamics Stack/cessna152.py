3cdcc70936e4b4ec7cda920ddd19740725632f05
Peter Sharpe <pds@mit.edu>
2022-06-10 11:48:19 -0400

    refactor to simplify function name for readability

--- tutorial/03 - Trajectory Optimization and Optimal Control/03 - The AeroSandbox Dynamics Stack/cessna152.py
+++ tutorial/03 - Trajectory Optimization and Optimal Control/03 - The AeroSandbox Dynamics Stack/cessna152.py
@@ -3,7 +3,7 @@ import aerosandbox.numpy as np
 from aerosandbox.tools import units as u
 
 
-def ft_to_m(feet, inches=0):  # Converts feet (and inches) to meters
+def ft(feet, inches=0):  # Converts feet (and inches) to meters
     return feet * u.foot + inches * u.inch
 
 
@@ -20,21 +20,21 @@ airplane = asb.Airplane(
             xsecs=[
                 asb.WingXSec(
                     xyz_le=[0, 0, 0],
-                    chord=ft_to_m(5, 4),
+                    chord=ft(5, 4),
                     airfoil=naca2412
                 ),
                 asb.WingXSec(
-                    xyz_le=[0, ft_to_m(7), ft_to_m(7) * np.sind(1)],
-                    chord=ft_to_m(5, 4),
+                    xyz_le=[0, ft(7), ft(7) * np.sind(1)],
+                    chord=ft(5, 4),
                     airfoil=naca2412
                 ),
                 asb.WingXSec(
                     xyz_le=[
-                        ft_to_m(4, 3 / 4) - ft_to_m(3, 8 + 1 / 2),
-                        ft_to_m(33, 4) / 2,
-                        ft_to_m(33, 4) / 2 * np.sind(1)
+                        ft(4, 3 / 4) - ft(3, 8 + 1 / 2),
+                        ft(33, 4) / 2,
+                        ft(33, 4) / 2 * np.sind(1)
                     ],
-                    chord=ft_to_m(3, 8 + 1 / 2),
+                    chord=ft(3, 8 + 1 / 2),
                     airfoil=naca0012
                 )
             ],
@@ -45,68 +45,68 @@ airplane = asb.Airplane(
             xsecs=[
                 asb.WingXSec(
                     xyz_le=[0, 0, 0],
-                    chord=ft_to_m(3, 8),
+                    chord=ft(3, 8),
                     airfoil=naca0012,
                     twist=-2
                 ),
                 asb.WingXSec(
-                    xyz_le=[ft_to_m(1), ft_to_m(10) / 2, 0],
-                    chord=ft_to_m(2, 4 + 3 / 8),
+                    xyz_le=[ft(1), ft(10) / 2, 0],
+                    chord=ft(2, 4 + 3 / 8),
                     airfoil=naca0012,
                     twist=-2
                 )
             ],
             symmetric=True
-        ).translate([ft_to_m(13, 3), 0, ft_to_m(-2)]),
+        ).translate([ft(13, 3), 0, ft(-2)]),
         asb.Wing(
             name="Vertical Stabilizer",
             xsecs=[
                 asb.WingXSec(
-                    xyz_le=[ft_to_m(-5), 0, 0],
-                    chord=ft_to_m(8, 8),
+                    xyz_le=[ft(-5), 0, 0],
+                    chord=ft(8, 8),
                     airfoil=naca0012,
                 ),
                 asb.WingXSec(
-                    xyz_le=[ft_to_m(0), 0, ft_to_m(1)],
-                    chord=ft_to_m(3, 8),
+                    xyz_le=[ft(0), 0, ft(1)],
+                    chord=ft(3, 8),
                     airfoil=naca0012,
                 ),
                 asb.WingXSec(
-                    xyz_le=[ft_to_m(0, 8), 0, ft_to_m(5)],
-                    chord=ft_to_m(2, 8),
+                    xyz_le=[ft(0, 8), 0, ft(5)],
+                    chord=ft(2, 8),
                     airfoil=naca0012,
                 ),
             ]
-        ).translate([ft_to_m(16, 11) - ft_to_m(3, 8), 0, ft_to_m(-2)])
+        ).translate([ft(16, 11) - ft(3, 8), 0, ft(-2)])
     ],
     fuselages=[
         asb.Fuselage(
             xsecs=[
                 asb.FuselageXSec(
-                    xyz_c=[0, 0, ft_to_m(-1)],
+                    xyz_c=[0, 0, ft(-1)],
                     radius=0,
                 ),
                 asb.FuselageXSec(
-                    xyz_c=[0, 0, ft_to_m(-1)],
-                    radius=ft_to_m(1.5)
+                    xyz_c=[0, 0, ft(-1)],
+                    radius=ft(1.5)
                 ),
                 asb.FuselageXSec(
-                    xyz_c=[ft_to_m(3), 0, ft_to_m(-0.85)],
-                    radius=ft_to_m(1.7)
+                    xyz_c=[ft(3), 0, ft(-0.85)],
+                    radius=ft(1.7)
                 ),
                 asb.FuselageXSec(
-                    xyz_c=[ft_to_m(5), 0, ft_to_m(0)],
-                    radius=ft_to_m(2.7)
+                    xyz_c=[ft(5), 0, ft(0)],
+                    radius=ft(2.7)
                 ),
                 asb.FuselageXSec(
-                    xyz_c=[ft_to_m(10, 4), 0, ft_to_m(0.3)],
-                    radius=ft_to_m(2.3)
+                    xyz_c=[ft(10, 4), 0, ft(0.3)],
+                    radius=ft(2.3)
                 ),
                 asb.FuselageXSec(
-                    xyz_c=[ft_to_m(21, 11), 0, ft_to_m(0.8)],
-                    radius=ft_to_m(0.3)
+                    xyz_c=[ft(21, 11), 0, ft(0.8)],
+                    radius=ft(0.3)
                 ),
             ]
-        ).translate([ft_to_m(-5), 0, ft_to_m(-3)])
+        ).translate([ft(-5), 0, ft(-3)])
     ]
 )
