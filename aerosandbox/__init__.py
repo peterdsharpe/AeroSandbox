4d3f92ce5f183ff1289ca3b42365ce936e072452
Peter Sharpe <pds@mit.edu>
2022-10-17 23:14:07 -0400

    version increment

--- aerosandbox/__init__.py
+++ aerosandbox/__init__.py
@@ -14,7 +14,7 @@ from aerosandbox.aerodynamics import *
 from aerosandbox.propulsion import *
 from aerosandbox.structures import *
 
-__version__ = "3.5.10"
+__version__ = "3.5.11"
 
 
 def docs():
