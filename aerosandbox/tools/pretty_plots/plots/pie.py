49294fa19dfe5c39fb356ef05c1e0d57a8e3a0c5
Peter Sharpe <pds@mit.edu>
2023-01-11 09:59:31 -0500

    add `sort_by` argument to pie()

--- aerosandbox/tools/pretty_plots/plots/pie.py
+++ aerosandbox/tools/pretty_plots/plots/pie.py
@@ -12,6 +12,7 @@ def pie(
         names: List[str],
         colors: Union[np.ndarray, List[str]] = None,
         label_format: Callable[[str, float, float], str] = lambda name, value, percentage: name,
+        sort_by: Union[np.ndarray, List[float], str, None] = None,
         startangle: float = 0.,
         center_text: str = None,
         x_labels: float = 1.25,
@@ -30,6 +31,28 @@ def pie(
     ### Check inputs
     if not len(names) == n_wedges:
         raise ValueError()  # TODO
+
+    ### Sort by
+    sort_by_error= ValueError('''Argument `sort_by` must be one of:\n
+    * a string of "values", "names", or "colors"
+    * an array of numbers corresponding to each pie slice, which will then be used for sorting
+    ''')
+
+    if sort_by is None:
+        sort_by = np.arange(n_wedges)
+    elif sort_by == "values":
+        sort_by=values
+    elif sort_by=="names":
+        sort_by=names
+    elif sort_by=="colors":
+        sort_by=colors # this might not make sense, depending on
+    elif isinstance(sort_by, str):
+        raise sort_by_error
+
+    order = np.argsort(sort_by)
+    names = np.array(names)[order]
+    values=np.array(values)[order]
+    colors=np.array(colors)[order]
 
     ### Compute percentages
     values = np.array(values).astype(float)
@@ -125,13 +148,38 @@ if __name__ == '__main__':
         values=list(data.values()),
         names=list(data.keys()),
         colors=[
-            p.palettes["categorical"][0] if s in ["USA"] else "gray"
+            "navy" if s in ["USA"] else "lightgray"
             for s in data.keys()
         ],
         label_format=lambda name, value, percentage: f"{name}, {eng_string(value)} ({percentage:.0f}%)",
         startangle=40,
         center_text="Majority of North\nAmerica's Population\nlives in USA"
     )
-    p.show_plot(
+    p.show_plot()
 
-    )
+#     import pandas as pd
+#     from io import StringIO
+#
+#     df = pd.read_csv(
+#         StringIO("""\
+# person,slices eaten,gender
+# alice,9,woman
+# bob,6,man
+# charlie,5,man
+# dan,8,man
+# eve,7,woman
+# frank,9,man
+# grace,4,woman
+# heidi,3,woman
+# """)
+#     )
+#
+#     fig, ax = plt.subplots(figsize=(8, 5))
+#     pie(
+#         values=df['slices eaten'],
+#         names=df['person'],
+#         colors=['blue' if g == 'man' else 'red' for g in df['gender']],
+#         label_format=lambda n, v, p: f"{n.capitalize()}, {v:.0g} ({p:.0f}%)",
+#         # sort_by=df[]
+#     )
+#     p.show_plot()
