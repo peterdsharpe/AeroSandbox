50c283cff1a77d6004ca456bbd3753f5496d7d52
Peter Sharpe <pds@mit.edu>
2023-01-31 22:12:07 -0500

    expand logic for is_casadi_type(). Add short-circuiting on common types

--- aerosandbox/numpy/determine_type.py
+++ aerosandbox/numpy/determine_type.py
@@ -1,30 +1,75 @@
-import casadi as cas
+import numpy as _onp
+import casadi as _cas
+from typing import Any
 
 
-def is_casadi_type(array_like, recursive=True) -> bool:
+def is_casadi_type(
+        object: Any,
+        recursive: bool = True
+) -> bool:
     """
-    Returns a boolean of whether an object is a CasADi data type or not. If the recursive flag is True, iterates recursively.
+    Returns a boolean of whether an object is a CasADi data type or not. If the recursive flag is True,
+    iterates recursively, returning True if any subelement (at any depth) is a CasADi type.
 
     Args:
 
         object: The object to evaluate.
 
         recursive: If the object is a list or tuple, recursively iterate through every subelement. If any of the
-        subelements are a CasADi type, return True. Otherwise, return False
+        subelements (at any depth) are a CasADi type, return True. Otherwise, returns False.
 
-    Returns: A boolean if the object is a CasADi data type.
+    Returns: A boolean if the object is (or contains, if recursive=True) a CasADi data type.
 
     """
-    if recursive and (isinstance(array_like, list) or isinstance(array_like, tuple)):
-        for element in array_like:
-            if is_casadi_type(element, recursive=True):
-                return True
+    t = type(object)
 
-    return (
-            isinstance(array_like, cas.MX) or
-            isinstance(array_like, cas.DM) or
-            isinstance(array_like, cas.SX)
-    )
+    # NumPy arrays cannot be or contain CasADi types, unless they are object arrays
+    if t == _onp.ndarray and object.dtype != 'O':
+        return False
+
+    # Skip certain Python types known not to be or contain CasADi types.
+    for type_to_skip in (
+            float, int, complex,
+            bool, str,
+            range,
+            type(None),
+            bytes, bytearray, memoryview
+    ):
+        if t == type_to_skip:
+            return False
+
+    # If it's directly a CasADi type, we're done.
+    if (
+            t == _cas.MX or
+            t == _cas.DM or
+            t == _cas.SX
+    ):
+        return True
+
+    # At this point, we know it's not a CasADi type, but we don't know if it *contains* a CasADi type (relevant if recursing)
+    if recursive:
+        if (
+                issubclass(t, list) or
+                issubclass(t, tuple) or
+                issubclass(t, set) or
+                (
+                        t == _onp.ndarray and object.dtype == 'O'
+                )
+        ):
+            for element in object:
+                if is_casadi_type(element, recursive=True):
+                    return True
+            return False
+
+        if issubclass(t, dict):
+            for kv in object.items():
+                if is_casadi_type(kv, recursive=True):
+                    return True
+            return False
+
+        return False
+    else:
+        return False
 
 
 def is_iterable(x):
