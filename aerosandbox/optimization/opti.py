262dfc4d09835a837256567df441d2feeb082529
Peter Sharpe <pds@mit.edu>
2023-02-01 12:43:32 -0500

    fix type hints for Python 3.7, 3.8

--- aerosandbox/optimization/opti.py
+++ aerosandbox/optimization/opti.py
@@ -1,4 +1,5 @@
 from typing import Union, List, Dict, Callable, Any
+import numpy.typing as npt
 import json
 import casadi as cas
 import aerosandbox.numpy as np
@@ -635,7 +636,7 @@ class Opti(cas.Opti):
         return sol
 
     def solve_sweep(self,
-                    parameter_mapping: Dict[cas.MX, np.ndarray[float]] = None,
+                    parameter_mapping: Dict[cas.MX, npt.NDArray[float]] = None,
                     update_initial_guesses_between_solves=False,
                     verbose=True,
                     solve_kwargs: Dict = None
