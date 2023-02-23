c3edeaf2ee67da9b11b15d9e6894fbe561d3eec4
Peter Sharpe <pds@mit.edu>
2023-02-22 23:15:41 -0500

    add plots

--- studies/ControlSurfaceEffectiveness/fit.py
+++ studies/ControlSurfaceEffectiveness/fit.py
@@ -29,4 +29,23 @@ fit = asb.FittedModel(
 
 print(fit.parameters)
 
-fit.plot()
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    import aerosandbox.tools.pretty_plots as p
+
+    x_plot = np.linspace(0, 1)
+    plt.plot(
+        x_plot, fit(x_plot)
+    )
+    plt.plot(
+        fit.x_data,
+        fit.y_data,
+        ".k",
+        alpha=0.1
+    )
+    plt.ylim(-0.1, 1.1)
+    p.show_plot(
+        "",
+        "Elevator Hinge Fraction",
+        "Effectiveness ($\\frac{d\\delta}{d\\alpha}$ at constant $C_L$)"
+    )
