76d069eb138dbe21682ec01cc60f3d5a61fe8aed
Peter Sharpe <peterdsharpe@gmail.com>
2023-03-17 14:34:06 -0400

    switch to python version flag

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
             'ipyvtklink >= 0.2',
             'trimesh >= 3',
             'sympy >= 1',
-            'cadquery >= 2; platform_system == "Windows"',
+            'cadquery >= 2; python_version >= "3.8"',
         ],
         "test": [
             'pytest',
