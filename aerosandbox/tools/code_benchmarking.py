04a9b723da1329e149ffc242fc17426ac4e19f8b
Peter Sharpe <pds@mit.edu>
2023-07-12 16:46:00 -0400

    Add more arguments to time_function()

--- aerosandbox/tools/code_benchmarking.py
+++ aerosandbox/tools/code_benchmarking.py
@@ -1,9 +1,13 @@
 import time
 from typing import Tuple, Any, Callable
+import aerosandbox.numpy as np
 
 
 def time_function(
-        func: Callable
+        func: Callable,
+        repeats: int = None,
+        desired_runtime: float = None,
+        runtime_reduction = np.min,
 ) -> Tuple[float, Any]:
     """
     Runs a given callable and tells you how long it took to run, in seconds. Also returns the result of the function
@@ -14,9 +18,42 @@ def time_function(
         - result is the result of the function, if any.
 
     """
-    start_ns = time.time_ns()
-    result = func()
+    if (repeats is not None) and (desired_runtime is not None):
+        raise ValueError("You can't specify both repeats and desired_runtime!")
+
+    def time_function_once():
+        start_ns = time.time_ns()
+        result = func()
+        return (
+            (time.time_ns() - start_ns) / 1e9,
+            result
+        )
+
+    runtimes = []
+
+    t, result = time_function_once()
+    runtimes.append(t)
+
+    if (desired_runtime is not None) and (repeats is None):
+        repeats = int(desired_runtime // t) - 1
+        # print(f"Running {func.__name__} {repeats} times to get a desired runtime of {desired_runtime} seconds.")
+
+    if repeats is None:
+        repeats = 0
+
+    for _ in range(repeats):
+        t, _ = time_function_once()
+        runtimes.append(t)
+
+
     return (
-        (time.time_ns() - start_ns) / 1e9,
+        runtime_reduction(runtimes),
         result
     )
+
+if __name__ == '__main__':
+
+    def f():
+        time.sleep(0.1)
+
+    print(time_function(f, desired_runtime=1))
