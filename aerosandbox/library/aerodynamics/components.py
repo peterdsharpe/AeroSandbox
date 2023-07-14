a4984861a91cad3c838bb6fd9f2b44640001ed9c
Peter Sharpe <peterdsharpe@gmail.com>
2023-07-14 11:48:17 -0400

    add CDA for control surface gaps and for bolts and rivets

--- aerosandbox/library/aerodynamics/components.py
+++ aerosandbox/library/aerodynamics/components.py
@@ -70,11 +70,69 @@ def CDA_control_linkage(
 
     return side_drag_multiplier * covered_drag_multiplier * linkage_length_multiplier * CDA_raw
 
-def CDA_control_surface_gaps():
-    pass
 
-def CDA_control_surface_hinges():
-    pass
+def CDA_control_surface_gaps(
+        local_chord,
+        control_surface_span,
+        local_thickness_over_chord=0.12,
+        control_surface_hinge_x=0.75,
+        n_side_gaps=2,
+        side_gap_width=None,
+        hinge_gap_width=None,
+):
+    if side_gap_width is None:
+        side_gap_width = np.maximum(
+            np.maximum(
+                0.002,
+                0.006 * local_chord
+            ),
+            control_surface_span * 0.01
+        )
+    if hinge_gap_width is None:
+        hinge_gap_width = 0.03 * local_chord
+
+    ### Chordwise gaps (at side edges of control surface)
+    """
+    Based on Hoerner, "Fluid Dynamic Drag", 1965, p. 5-13. Figure 26, "Drag of longitudinal wing gaps, 
+    tested on 2412 airfoil at C_L = 0.1 and Re_c = 2 * 10^6"
+    """
+
+    CDA_side_gaps = n_side_gaps * (side_gap_width * local_chord * local_thickness_over_chord) * 0.50
+
+    ### Spanwise gaps (at hinge line of control surface)
+    """
+    Based on Hoerner, "Fluid Dynamic Drag", 1965, p. 5-13. Figure 27, "Evaluation of drag due to control gaps"
+    """
+
+    CDA_hinge_gap = 0.025 * hinge_gap_width * control_surface_span
+
+    ### Total
+    return CDA_side_gaps + CDA_hinge_gap
+
+
+def CDA_bolts_and_rivets(
+        diameter: float,
+        kind: str = "flush_rivet"
+):
+    S_ref = np.pi * diameter ** 2 / 4
+
+    if kind == "flush_rivet":
+        CDA = 0.002 * S_ref
+    elif kind == "round_rivet":
+        CDA = 0.04 * S_ref
+    elif kind == "flat_head_bolt":
+        CDA == 0.02 * S_ref
+    elif kind == "round_head_bolt":
+        CDA == 0.32 * S_ref
+    elif kind == "cylindrical_bolt":
+        CDA == 0.42 * S_ref
+    elif kind == "hex_bolt":
+        CDA == 0.80 * S_ref
+    else:
+        raise ValueError("Invalid `kind` of bolt or rivet.")
+
+    return CDA
+
 
 def CDA_pitot_static_tube():
     pass
