4c427dfc7ca98712f359325a23f44f83cb0ed9a0
Peter Sharpe <peterdsharpe@gmail.com>
2023-08-07 11:44:15 -0400

    add electric aircraft cost model

--- aerosandbox/library/costs.py
+++ aerosandbox/library/costs.py
@@ -187,3 +187,237 @@ def modified_DAPCA_IV_production_cost_analysis(
     costs["total"] = sum(costs.values())
 
     return costs
+
+
+def electric_aircraft_direct_operating_cost_analysis(
+        production_cost_per_airframe: float,
+        nominal_cruise_airspeed: float,
+        nominal_mission_range: float,
+        battery_capacity: float,
+        num_passengers_nominal: int,
+        num_crew: int = 1,
+        battery_fraction_used_on_nominal_mission: float = 0.8,
+        typical_passenger_utilization: float = 0.8,
+        flight_hours_per_year: float = 1200,
+        airframe_lifetime_years: float = 20,
+        airframe_eol_resale_value_fraction: float = 0.4,
+        battery_cost_per_kWh_capacity: float = 150.0,
+        battery_cycle_life: float = 1500,
+        real_interest_rate: float = 0.04,
+        electricity_cost_per_kWh: float = 0.145,
+        annual_expenses_per_crew: float = 100000 * 1.5,
+        ascent_time: float = 0.2 * u.hour,
+        descent_time: float = 0.2 * u.hour,
+) -> Dict[str, float]:
+    """
+    Estimates the overall operating cost of an electric aircraft. Includes both direct and indirect operating costs.
+
+    Here, direct operating costs (DOC) are taken to include the following costs:
+
+    - Airframe depreciation
+    - Airframe financing
+    - Insurance
+    - Maintenance
+    - Battery replacement
+    - Energy costs (here, electricity)
+    - Cockpit and cabin crew costs
+    - Airport landing, terminal, and handling fees
+
+    Any costs that are not included here are considered indirect costs. These indirect costs would include,
+    but are not limited to: advertisement, administrative costs, depreciation of non-airframe assets, and taxes.
+
+    Airport fees estimated for the Phoenix-Mesa Gateway Airport, due to public availability of the fee schedule:
+    https://www.gatewayairport.com/documents/documentlibrary/wgaa%20organizational%20documents/airport%20rates%20charges%20-%20effective%20march%201,%202017.pdf
+
+    Args:
+
+        production_cost_per_airframe: The cost to produce a single airframe, in present-day dollars.
+
+        nominal_cruise_airspeed: The nominal cruise airspeed of the aircraft, in m/s.
+
+        nominal_mission_range: The nominal mission range of the aircraft, in meters.
+
+        battery_capacity: The total capacity of the battery, in Joules.
+
+        num_passengers_nominal: The number of passengers that the aircraft is designed to carry.
+
+        num_crew: The number of crew members required to operate the aircraft.
+
+        battery_fraction_used_on_nominal_mission: The fraction of the battery's capacity that is used on the nominal
+            mission.
+
+        typical_passenger_utilization: The fraction of the aircraft's passenger capacity that is typically utilized.
+
+        flight_hours_per_year: The number of flight hours per year that the aircraft is expected to fly.
+
+        airframe_lifetime_years: The number of years that the airframe is expected to last. After this time, the airframe
+            is assumed to be sold at some lower reasle value.
+
+        airframe_eol_resale_value_fraction: The expect resale value of the airframe at the end of its lifetime,
+            expressed as a fraction of the airframe's production cost.
+
+        battery_cost_per_kWh_capacity: The cost of the battery, per kWh of capacity, in present-day dollars.
+
+        battery_cycle_life: The number of charge/discharge cycles that the battery is expected to last before full
+            replacement is required.
+
+        real_interest_rate: The real interest rate per year. This is the interest rate minus the inflation rate. This is
+            used to calculate the present-day value of future costs (e.g., airframe financing).
+
+        electricity_cost_per_kWh: The cost of electricity, per kWh, in present-day dollars.
+
+        annual_expenses_per_crew: The annual expenses per crew member, in present-day dollars. Should include the total
+            burdened cost of the crew member, including salary, benefits, and other expenses.
+
+        ascent_time: The time required to ascend to cruise altitude, in seconds.
+
+        descent_time: The time required to descend from cruise altitude, in seconds.
+
+    Returns:
+        A dictionary of operating costs per passenger-mile, in present-day dollars, with the following keys:
+
+            * "airframe_depreciation"
+            * "airframe_financing"
+            * "insurance"
+            * "airframe_maintenance"
+            * "propulsion_maintenance"
+            * "battery_replacement"
+            * "energy"
+            * "crew"
+            * "airport_landing_fees"
+            * "airport_terminal_fees"
+            * "airport_parking_fees"
+            * "airport_passenger_facility_charge"
+            * "indirect"
+
+        One key, "total", is also included, which is the sum of all of the above costs.
+
+    """
+
+    ### Calculate per-mission parameters
+    ascent_and_descent_airspeed = 0.75 * nominal_cruise_airspeed
+    ascent_and_descent_time = np.softmin(
+        ascent_time + descent_time,
+        nominal_mission_range / ascent_and_descent_airspeed,
+        softness=2 * u.minute,
+    )
+    ascent_and_descent_distance = ascent_and_descent_airspeed * ascent_and_descent_time
+    cruise_distance = nominal_mission_range - ascent_and_descent_distance
+    cruise_time = cruise_distance / nominal_cruise_airspeed
+    mission_time = ascent_and_descent_time + cruise_time
+
+    ### Calculate annual utilization parameters
+    flights_per_year = flight_hours_per_year / (mission_time / u.hour)
+
+    ### Begin calculating costs on a per-flight basis
+    costs_per_flight = dict()  # Lists the cost per flight for each cost category
+
+    ### Airframe depreciation costs
+    num_airframe_flights_lifetime = flights_per_year * airframe_lifetime_years
+    net_value_per_airframe_over_lifetime = (
+            production_cost_per_airframe  # Production cost
+            - (production_cost_per_airframe * airframe_eol_resale_value_fraction)  # End-of-life resale value
+    )
+    costs_per_flight["airframe_depreciation"] = (
+            net_value_per_airframe_over_lifetime / num_airframe_flights_lifetime
+    )
+
+    ### Airframe financing cost
+    airframe_financing_lifetime_cost = production_cost_per_airframe * (
+            np.exp(real_interest_rate * airframe_lifetime_years) - 1
+    )
+    costs_per_flight["airframe_financing"] = (
+            airframe_financing_lifetime_cost / num_airframe_flights_lifetime
+    )
+
+    ### Insurance cost
+    insurance_cost_per_year = production_cost_per_airframe * (
+            0.025  # Base rate of 2% on average, adjusted higher for perceived higher risk of new electric technology
+            * ((num_passengers_nominal + num_crew + 1) / 11) ** 0.5  # Adj. for number of souls on board; but sublinear
+            * (flight_hours_per_year / 1200) ** 0.5  # Normalizing to industry average; but again, sublinear
+    )
+    costs_per_flight["insurance"] = insurance_cost_per_year / flights_per_year
+
+    ### Airframe maintenance cost
+    # maintenance_cost_per_year = production_cost_per_airframe * 0.04
+    # costs_per_flight["maintenance"] = maintenance_cost_per_year / flights_per_year
+    costs_per_flight["airframe_maintenance"] = (production_cost_per_airframe / 3e6) * (
+            65 * (mission_time / u.hour) +
+            (65)  # per cycle
+    )
+
+    ### Propulsion maintenance cost
+    costs_per_flight["propulsion_maintenance"] = (production_cost_per_airframe / 3e6) * (
+            (58 * (mission_time / u.hour)) +
+            (50)  # per cycle
+    )
+
+    ### Battery replacement cost
+    battery_capacity_kWh = battery_capacity / (u.kilo * u.watt_hour)
+    battery_cost = battery_capacity_kWh * battery_cost_per_kWh_capacity
+    costs_per_flight["battery_replacement"] = battery_cost / battery_cycle_life
+
+    ### Energy Cost
+    electric_energy_per_flight = battery_capacity * battery_fraction_used_on_nominal_mission
+    electric_energy_per_flight_kWh = electric_energy_per_flight / (u.kilo * u.watt_hour)
+    costs_per_flight["energy"] = electric_energy_per_flight_kWh * electricity_cost_per_kWh
+
+    ### Crew cost
+    costs_per_flight["crew"] = (
+            num_crew *
+            annual_expenses_per_crew /
+            flights_per_year
+    )
+
+    ### Airport landing fees
+    estimated_max_gross_landing_weight = (  # Model is very approximate, because landing fees are small
+            production_cost_per_airframe / 266  # typical cost per kg empty weight
+            * 1.2  # Rough ratio of landing weight to empty weight
+    )
+    costs_per_flight["airport_landing_fees"] = (
+            1.20 * estimated_max_gross_landing_weight / 1000
+    )
+
+    ### Airport terminal use fees
+    costs_per_flight["airport_terminal_fees"] = (
+        50  # Cost per turn-around
+    )
+
+    ### Airport aircraft parking fees
+    costs_per_flight["airport_parking_fees"] = (
+        35  # Cost per turn-around
+    )
+
+    ### Airport passenger facility charge
+    costs_per_flight["airport_passenger_facility_charge"] = (
+            4.50 * num_passengers_nominal
+    )
+
+    ### Indirect Costs
+    costs_per_flight["indirect"] = 0.2 * sum(costs_per_flight.values())
+
+    # Cost per passenger mile
+    n_passengers_actual = num_passengers_nominal * typical_passenger_utilization
+    passenger_miles_per_flight = n_passengers_actual * (nominal_mission_range / u.mile)
+    costs_per_flight["total"] = sum(costs_per_flight.values())
+
+    # Return same dictionary as before
+    costs_per_paxmi = {
+        k: v / passenger_miles_per_flight
+        for k, v in costs_per_flight.items()
+    }
+
+    return costs_per_paxmi
+
+
+if __name__ == '__main__':
+    res = electric_aircraft_direct_operating_cost_analysis(
+        production_cost_per_airframe=3.0e6,
+        nominal_cruise_airspeed=250 * u.knot,
+        nominal_mission_range=150 * u.mile,
+        battery_capacity=800e3 * u.watt_hour,
+        num_passengers_nominal=9,
+    )
+    import pandas as pd
+
+    print(pd.Series(res))
