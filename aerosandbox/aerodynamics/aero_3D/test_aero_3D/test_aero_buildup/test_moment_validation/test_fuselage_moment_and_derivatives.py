e314ec2f8254892e50633658c8c35ed4355d3927
Peter <pds@mit.edu>
2023-08-09 10:47:10 -0400

    add test for cambered fuselage performance

--- aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_aero_buildup/test_moment_validation/test_fuselage_moment_and_derivatives.py
+++ aerosandbox/aerodynamics/aero_3D/test_aero_3D/test_aero_buildup/test_moment_validation/test_fuselage_moment_and_derivatives.py
@@ -185,11 +185,70 @@ def test_derivatives_at_incidence(
     )
 
 
+def test_cambered_fuselage(
+        AeroAnalysis: Type = asb.AeroBuildup,
+):
+    fuselage = asb.Fuselage(
+        xsecs=[
+            asb.FuselageXSec(
+                xyz_c=[
+                    xi,
+                    0,
+                    xi * (1 - xi) * 4 * 0.15
+                ],
+                radius=xi ** 0.5 * (1 - xi) * 0.2
+            )
+            for xi in np.sinspace(0, 1, 20)
+        ]
+    )
+    # fuse.draw_three_view()
+
+    airplane = asb.Airplane(
+        fuselages=[fuselage],
+        s_ref=1,
+        c_ref=1,
+        b_ref=1,
+    )
+
+    analysis = AeroAnalysis(
+        airplane=airplane,
+        op_point=asb.OperatingPoint(
+            velocity=100,
+            alpha=5,
+            beta=5,
+        ),
+        xyz_ref=np.array([0.5, 0, 0]),
+    )
+
+    try:
+        aero = analysis.run_with_stability_derivatives()
+    except AttributeError:
+        aero = analysis.run()
+
+    print(f"Aerodynamic coefficients with {AeroAnalysis.__name__}:")
+    for key in ["Cm", "Cma", "Cn", "Cnb"]:
+        print(f"{key.rjust(10)}: {aero[key]:20.4f}")
+
+    # assert aero["Cm"] == pytest.approx(0, abs=1e-3)
+    # assert aero["Cn"] == pytest.approx(0, abs=1e-3)
+
+    assert aero["Cma"] == pytest.approx(
+        2 * fuselage.volume() / airplane.s_ref / airplane.c_ref,
+        rel=0.5 # this is not-strictly-speaking exact, hence the large tolerance
+    )
+    assert aero["Cnb"] == pytest.approx(
+        -2 * fuselage.volume() / airplane.s_ref / airplane.b_ref,
+        rel=0.5 # this is not-strictly-speaking exact, hence the large tolerance
+    )
+
+
 if __name__ == '__main__':
-    asb.Airplane(fuselages=[make_fuselage()]).draw_three_view()
-    # test_derivatives_at_zero_geometric_angles(asb.AVL)
-    test_derivatives_at_zero_geometric_angles(asb.AeroBuildup)
-    # test_derivatives_at_constant_offset(asb.AVL)
-    test_derivatives_at_constant_offset(asb.AeroBuildup)
-    # test_derivatives_at_incidence(asb.AVL)
-    test_derivatives_at_incidence(asb.AeroBuildup)
+    # asb.Airplane(fuselages=[make_fuselage()]).draw_three_view()
+    # # test_derivatives_at_zero_geometric_angles(asb.AVL)
+    # test_derivatives_at_zero_geometric_angles(asb.AeroBuildup)
+    # # test_derivatives_at_constant_offset(asb.AVL)
+    # test_derivatives_at_constant_offset(asb.AeroBuildup)
+    # # test_derivatives_at_incidence(asb.AVL)
+    # test_derivatives_at_incidence(asb.AeroBuildup)
+    # # test_cambered_fuselage(asb.AVL)
+    # test_cambered_fuselage(asb.AeroBuildup)
