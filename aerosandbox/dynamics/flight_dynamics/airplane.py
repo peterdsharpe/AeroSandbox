ffbb9fe312ee492485a1bed2543478a1ae2e2067
Peter Sharpe <peterdsharpe@gmail.com>
2023-09-01 15:32:53 -0400

    Add calculations for short-period and phugoid modes

--- aerosandbox/dynamics/flight_dynamics/airplane.py
+++ aerosandbox/dynamics/flight_dynamics/airplane.py
@@ -22,22 +22,30 @@ def get_modes(
     Izz = mass_props.Izz
     u0 = op_point.velocity
 
-    # X_u = QS / m / u0 * Cxu
+    Cxu = -2 * aero["CD"]
+    Czu = -2 * aero["CL"]
+
+    Cmq = aero["Cmq"]
+    Cma = aero["Cma"]
+
+    X_u = QS / m / u0 * Cxu  # Units: 1/sec
     # X_w = QS / m / u0 * Cxa
     # X_q = QS / m * c / (2 * u0) * Cxq
-    # Z_u = QS / m / u0 * Czu
+    Z_u = QS / m / u0 * Czu  # Units: 1/sec
     # Z_w = QS / m / u0 * Cza
     # Z_q = QS / m * c / (2 * u0) * Czq
     # M_u = QS * c / Iyy / u0 * Cmu
-    # M_w = QS * c / Iyy / u0 * Cma
-    # M_q = QS * c / Iyy * c / (2 * u0) * Cmq
+    M_w = QS * c / Iyy / u0 * Cma # Units: 1/(meter * sec)
+    M_q = QS * c / Iyy * c / (2 * u0) * Cmq # Units: 1/sec
+
+    modes = {}
 
     def get_mode_info(
             sigma,
             omega_squared,
     ):
         is_oscillatory = omega_squared > 0
-        mode_info = {
+        return {
             "eigenvalue_real": sigma + np.where(
                 is_oscillatory,
                 0,
@@ -49,46 +57,39 @@ def get_modes(
                 0,
             ),
         }
-        mode_info['damping_ratio'] = (
-                -mode_info['eigenvalue_real'] /
-                (mode_info['eigenvalue_real'] ** 2 + mode_info['eigenvalue_imag'] ** 2) ** 0.5
-        )
-        return mode_info
-
-    modes = {}
 
     ##### Longitudinal modes
 
-    # TODO add longitudinal
-    # ### Phugoid
-    # modes['phugoid'] = get_mode_info(
-    #     sigma=0.5 * X_u,
-    #     omega_squared=-(X_u ** 2) / 4 - g * Z_u / u0
-    # )
-    # modes['phugoid'] = {
-    #     "frequency_approx"    : 2 ** 0.5 * g / u0,
-    #     "damping_ratio_approx": 2 ** -0.5 * CD / CL
-    # }
+    ### Phugoid
+    modes['phugoid'] = {  # FVA, Eq. 9.55-9.57
+        **get_mode_info(
+            sigma=X_u / 2,
+            omega_squared=-(X_u ** 2) / 4 - g * Z_u / u0
+        ),
+        "eigenvalue_imag_approx": 2 ** 0.5 * g / u0,
+        "damping_ratio_approx"  : 2 ** -0.5 * aero["CD"] / aero["CL"],
+    }
     #
     # ### Short-period
-    # modes['short_period'] = get_mode_info(
-    #     sigma=0.5 * M_q,
-    #     omega_squared=-(M_q ** 2) / 4 - u0 * M_w
-    # )
+    modes['short_period'] = get_mode_info(
+        sigma=0.5 * M_q,
+        omega_squared=-(M_q ** 2) / 4 - u0 * M_w
+    )
 
     ##### Lateral modes
 
     ### Roll subsidence
-    modes['roll_subsidence'] = {
+    modes['roll_subsidence'] = {  # FVA, Eq. 9.63
         "eigenvalue_real": (
                 QS * b ** 2 / (2 * Ixx * u0) * aero["Clp"]
         ),
         "eigenvalue_imag": 0,
-        "damping_ratio"  : 0,
+        "damping_ratio"  : 1,
     }
 
     ### Dutch roll
-    modes['dutch_roll'] = get_mode_info(
+
+    modes['dutch_roll'] = get_mode_info(  # FVA, Eq. 9.68
         sigma=(
                 QS * b ** 2 /
                 (2 * Izz * u0) *
@@ -106,14 +107,21 @@ def get_modes(
     )
 
     ### Spiral
-    modes['spiral'] = {
+    spiral_parameter = (aero["Cnr"] - aero["Cnb"] * aero["Clr"] / aero["Clb"])  # FVA, Eq. 9.66
+    modes['spiral'] = {  # FVA, Eq. 9.66
         "eigenvalue_real": (
                 QS * b ** 2 / (2 * Izz * u0) *
-                (aero["Cnr"] - aero["Cnb"] * aero["Clr"] / aero["Clb"])
+                spiral_parameter
         ),
         "eigenvalue_imag": 0,
-        "damping_ratio"  : 0,
     }
+
+    ### Compute damping ratios of all modes
+    for mode_name, mode_data in modes.items():
+        modes[mode_name]['damping_ratio'] = (
+                -mode_data['eigenvalue_real'] /
+                (mode_data['eigenvalue_real'] ** 2 + mode_data['eigenvalue_imag'] ** 2) ** 0.5
+        )
 
     return modes
 
@@ -124,44 +132,55 @@ if __name__ == '__main__':
 
     from pprint import pprint
 
+    airplane = asb.Airplane(
+        s_ref=9,
+        c_ref=0.90,
+        b_ref=10,
+    )
+    op_point = asb.OperatingPoint(velocity=10)
+    aero = dict(
+        CL=0.46,
+        CD=0.11,
+        Cm=0.141,
+        CLa=5.736,
+        # CYa = 0,
+        # Cla = 0,
+        Cma=-1.59,
+        # Cna = 0,
+        # CLb = 0,
+        CYb=-0.380,
+        Clb=-0.208,
+        # Cmb=0,
+        Cnb=0.0294,
+        # CLp =0,
+        CYp=-0.325,
+        Clp=-0.593,
+        # Cmp=0,
+        Cnp=-0.041,
+        CLq=10.41,
+        # CYq=0,
+        # Clq=0,
+        Cmq=-25.05,
+        # Cnq=0,
+        # CLr=0,
+        CYr=0.194,
+        Clr=0.143,
+        # Cmr=0,
+        Cnr=-0.048
+    )
+
+    mass_props = asb.mass_properties_from_radius_of_gyration(
+        mass=op_point.dynamic_pressure() * airplane.s_ref * aero["CL"] / 9.81,
+        radius_of_gyration_x=0.5 * airplane.b_ref,
+        radius_of_gyration_y=3 * airplane.c_ref,
+        radius_of_gyration_z=0.5 * airplane.b_ref,
+    )
+
     pprint(
         get_modes(
-            airplane=asb.Airplane(
-                s_ref=9,
-                c_ref=0.90,
-                b_ref=10,
-            ),
-            op_point=asb.OperatingPoint(velocity=10),
-            mass_props=asb.MassProperties(mass=1, Ixx=1, Iyy=1, Izz=1),
-            aero=dict(
-                CL=0.46,
-                CD=0.11,
-                Cm=0.141,
-                CLa=5.736,
-                # CYa = 0,
-                # Cla = 0,
-                Cma=-1.59,
-                # Cna = 0,
-                # CLb = 0,
-                CYb=-0.380,
-                Clb=-0.208,
-                # Cmb=0,
-                Cnb=0.0294,
-                # CLp =0,
-                CYp=-0.325,
-                Clp=-0.593,
-                # Cmp=0,
-                Cnp=-0.041,
-                CLq=10.41,
-                # CYq=0,
-                # Clq=0,
-                Cmq=-25.05,
-                # Cnq=0,
-                # CLr=0,
-                CYr=0.194,
-                Clr=0.143,
-                # Cmr=0,
-                Cnr=-0.048
-            )
+            airplane=airplane,
+            op_point=op_point,
+            mass_props=mass_props,
+            aero=aero
         )
     )
