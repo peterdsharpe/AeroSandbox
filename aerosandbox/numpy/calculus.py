a5ff9fd0646d957f70a82ae895e6b0c4bddd20a8
Peter Sharpe <peterdsharpe@gmail.com>
2024-01-01 14:44:25 +0100

    adds a working np.gradient

--- aerosandbox/numpy/calculus.py
+++ aerosandbox/numpy/calculus.py
@@ -3,6 +3,7 @@ import casadi as _cas
 from aerosandbox.numpy.determine_type import is_casadi_type
 from aerosandbox.numpy.conditionals import where as _where
 from aerosandbox.numpy.arithmetic_dyadic import mod as _mod
+from aerosandbox.numpy.array import array, concatenate
 
 
 def diff(a, n=1, axis=-1, period=None):
@@ -33,6 +34,153 @@ def diff(a, n=1, axis=-1, period=None):
         return result
 
 
+def gradient(
+        f,
+        *varargs,
+        axis=None,
+        edge_order=1
+):
+    if not is_casadi_type(f) and all([not is_casadi_type(vararg) for vararg in varargs]):
+        return _onp.gradient(
+            f,
+            *varargs,
+            axis=axis,
+            edge_order=edge_order
+        )
+    else:
+        f = array(f)
+        shape = f.shape
+
+        # Handle the varargs argument
+        if len(varargs) == 0:
+            varargs = (1.,)
+
+        if len(varargs) == 1:
+            varargs = [varargs[0] for i in range(len(shape))]
+
+        if len(varargs) != len(shape):
+            raise ValueError("You must specify either 0, 1, or N varargs, where N is the number of dimensions of f.")
+        else:
+            dxes = []
+
+            for i, vararg in enumerate(varargs):
+                if np.prod(array(vararg).shape) == 1:  # If it's a scalar, you have dx values
+                    dxes.append(
+                        vararg * _onp.ones(shape[i] - 1)
+                    )
+                else:
+                    dxes.append(
+                        diff(vararg)
+                    )
+
+        # Handle the axis argument, with the edge case the CasADi arrays are always 2D
+        if axis is None:
+            if is_casadi_type(f, recursive=False) and shape[1] == 1:
+                axis = 0
+            elif len(shape) <= 1:
+                axis = 0
+            else:
+                axis = tuple(_onp.arange(len(shape)))
+
+        try:
+            tuple(axis)  # See if axis is iterable
+            axis_is_iterable = True
+        except TypeError:
+            axis_is_iterable = False
+
+        if axis_is_iterable:
+            return [
+                gradient(
+                    f,
+                    *varargs,
+                    axis=axis_i,
+                    edge_order=edge_order
+                )
+                for axis_i in axis
+            ]
+
+        else:
+            # Check validity of axis
+            if axis < 0:
+                axis = len(shape) + axis
+            if is_casadi_type(f, recursive=False):
+                if axis not in [0, 1]:
+                    raise ValueError("axis must be 0 or 1 for CasADi arrays.")
+
+            dx = dxes[axis]
+            dx_shape = [1] * len(shape)
+            dx_shape[axis] = shape[axis] - 1
+            dx = np.reshape(dx, dx_shape)
+
+            def get_slice(slice_obj: slice):
+                slices = [slice(None)] * len(shape)
+                slices[axis] = slice_obj
+                return tuple(slices)
+
+            hm = dx[get_slice(slice(None, -1))]
+            hp = dx[get_slice(slice(1, None))]
+
+            dfp = (
+                    f[get_slice(slice(2, None))]
+                    - f[get_slice(slice(1, -1))]
+            )
+            dfm = (
+                    f[get_slice(slice(1, -1))]
+                    - f[get_slice(slice(None, -2))]
+            )
+
+            grad_f = (
+                             hm ** 2 * dfp + hp ** 2 * dfm
+                     ) / (
+                             hm * hp * (hm + hp)
+                     )
+
+            if edge_order == 1:
+                # First point
+                df_f = dfm[get_slice(slice(0, 1))]
+                h_f = hm[get_slice(slice(0, 1))]
+                grad_f_first = df_f / h_f
+
+                # Last point
+                df_l = dfp[get_slice(slice(-1, None))]
+                h_l = hp[get_slice(slice(-1, None))]
+                grad_f_last = df_l / h_l
+
+            elif edge_order == 2:
+                # First point
+                dfm_f = dfm[get_slice(slice(0, 1))]
+                dfp_f = dfp[get_slice(slice(0, 1))]
+                hm_f = hm[get_slice(slice(0, 1))]
+                hp_f = hp[get_slice(slice(0, 1))]
+                grad_f_first = (
+                                       2 * dfm_f * hm_f * hp_f + dfm_f * hp_f ** 2 - dfp_f * hm_f ** 2
+                               ) / (
+                                       hm_f * hp_f * (hm_f + hp_f)
+                               )
+
+                # Last point
+                dfm_l = dfm[get_slice(slice(-1, None))]
+                dfp_l = dfp[get_slice(slice(-1, None))]
+                hm_l = hm[get_slice(slice(-1, None))]
+                hp_l = hp[get_slice(slice(-1, None))]
+                grad_f_last = (
+                                      -dfm_l * hp_l ** 2 + dfp_l * hm_l ** 2 + 2 * dfp_l * hm_l * hp_l
+                              ) / (
+                                      hm_l * hp_l * (hm_l + hp_l)
+                              )
+
+            else:
+                raise ValueError("Invalid edge_order.")
+
+            grad_f = concatenate((
+                grad_f_first,
+                grad_f,
+                grad_f_last
+            ), axis=axis)
+
+            return grad_f
+
+
 def trapz(x, modify_endpoints=False):  # TODO unify with NumPy trapz, this is different
     """
     Computes each piece of the approximate integral of `x` via the trapezoidal method with unit spacing.
@@ -45,6 +193,12 @@ def trapz(x, modify_endpoints=False):  # TODO unify with NumPy trapz, this is di
         starting at index i.
 
     """
+    import warnings
+    warnings.warn(
+        "trapz() will eventually be deprecated, since NumPy plans to remove it in the upcoming NumPy 2.0 release (2024). \n"
+        "For discrete intervals, use asb.numpy.integrate_discrete_intervals(f, method=\"trapz\") instead.",
+        PendingDeprecationWarning)
+
     integral = (
                        x[1:] + x[:-1]
                ) / 2
@@ -58,5 +212,20 @@ def trapz(x, modify_endpoints=False):  # TODO unify with NumPy trapz, this is di
 if __name__ == '__main__':
     import numpy as np
 
-    a = np.linspace(-500, 500, 21) % 360 - 180
-    print(diff(a, period=360))
+    # a = np.linspace(-500, 500, 21) % 360 - 180
+    # print(diff(a, period=360))
+
+    x = np.cumsum(np.arange(10))
+    y = x ** 2
+
+    # print(gradient(y, x, edge_order=1))
+    # print(gradient(y, x, method="quadratic", edge_order=1))
+
+    from aerosandbox.tools.code_benchmarking import Timer
+    import casadi as cas
+
+
+    with Timer("np"):
+        print(gradient(np.eye(50), 2, 1))
+    with Timer("custom"):
+        print(gradient(cas.MX_eye(50), 2, 1))
