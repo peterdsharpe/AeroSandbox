12294204f183108989cc7f9a4632702a5aa77284
Peter Sharpe <pds@mit.edu>
2024-04-05 19:28:01 -0400

    modernize code with asb.Opti

--- studies/AtmosphereFitting_v1/optimal_interpolation.py
+++ studies/AtmosphereFitting_v1/optimal_interpolation.py
@@ -3,34 +3,31 @@ Suppose you have some function f(x)=exp(-x) on the range x in [0, 1].
 
 You want to pick n points to linearly interpolate this such that the L2-error is minimized. How do you do it?
 """
-import numpy as np
-import casadi as cas
+import aerosandbox as asb
+import aerosandbox.numpy as np
 import matplotlib.pyplot as plt
 
-opti = cas.Opti()
+opti = asb.Opti()
 
 n = 101
 
-x = opti.variable(n)  # cas.linspace(0,1,n)
-
-opti.set_initial(x, cas.linspace(0, 10, n))
+x = opti.variable(np.linspace(0, 10, n))
 
 opti.subject_to([
     x[0] == 0,
     x[-1] == 10,
 ])
 
-y = cas.exp(-x)
+y = np.exp(-x)
 
 x1 = x[:-1]
 x2 = x[1:]
-errors = -x1 * cas.exp(-x2) / 2 - x1 * cas.exp(-x1) / 2 + x2 * cas.exp(-x2) / 2 + x2 * cas.exp(-x1) / 2 + cas.exp(
-    -x2) - cas.exp(-x1)
-error = cas.sum1(errors)
+errors = -x1 * np.exp(-x2) / 2 - x1 * np.exp(-x1) / 2 + x2 * np.exp(-x2) / 2 + x2 * np.exp(-x1) / 2 + np.exp(
+    -x2) - np.exp(-x1)
+error = np.sum(errors)
 
 opti.minimize(error * 1e3)
-opti.solver('ipopt')
 sol = opti.solve()
 
-xopt = sol.value(x)
-minerror = sol.value(error)
+xopt = sol(x)
+minerror = sol(error)
