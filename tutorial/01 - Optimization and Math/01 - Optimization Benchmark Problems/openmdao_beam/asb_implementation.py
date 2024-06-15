611e3df3a8a1e762b1449d85f5ef7e92e50fe330
Peter Sharpe <pds@mit.edu>
2024-06-15 19:58:48 -0400

    improves benchmark code readability

--- tutorial/01 - Optimization and Math/01 - Optimization Benchmark Problems/openmdao_beam/asb_implementation.py
+++ tutorial/01 - Optimization and Math/01 - Optimization Benchmark Problems/openmdao_beam/asb_implementation.py
@@ -6,18 +6,18 @@ E = 1e3  # Elastic modulus [N/m^2]
 L = 1  # Beam length [m]
 b = 0.1  # Beam width [m]
 volume = 0.01  # Total material allowed [m^3]
-tip_load = -1  # Tip load [N]
+tip_load = 1  # Tip load [N]
 
 x = np.linspace(0, L, N)  # Node locations [m]
 
 opti = asb.Opti()
-h = opti.variable(init_guess=np.ones(N), lower_bound=1e-2)
-I = b * h ** 3 / 12  # Bending moment of inertia [m^4]
+h = opti.variable(init_guess=np.ones(N), lower_bound=1e-6)
+I = (1 / 12) * b * h ** 3  # Bending moment of inertia [m^4]
 
-V = np.ones(N) * -tip_load  # Shear force [N]
+V = np.ones(N) * (-tip_load)  # Shear force [N]
 M = opti.variable(init_guess=np.zeros(N))  # Moment [N*m]
-th = opti.variable(init_guess=np.zeros(N), scale=E)  # Slope [rad]
-w = opti.variable(init_guess=np.zeros(N), scale=E)  # Displacement [m]
+th = opti.variable(init_guess=np.zeros(N))  # Slope [rad]
+w = opti.variable(init_guess=np.zeros(N))  # Displacement [m]
 
 opti.subject_to([  # Governing equations
     np.diff(M) == np.trapz(V) * np.diff(x),
@@ -29,12 +29,10 @@ opti.subject_to([  # Boundary conditions
     th[0] == 0,
     w[0] == 0,
 ])
-opti.subject_to(np.mean(h * b) <= volume / L)
-opti.minimize(tip_load * w[-1])
+opti.subject_to(np.mean(h * b) <= volume / L)  # Volume constraint
+opti.minimize(w[-1])  # Objective: minimize tip deflection
 sol = opti.solve()
-
-print(sol(h))
-print(sol(tip_load * w[-1]))
+print(sol(h))  # Gives the optimized beam thickness
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
