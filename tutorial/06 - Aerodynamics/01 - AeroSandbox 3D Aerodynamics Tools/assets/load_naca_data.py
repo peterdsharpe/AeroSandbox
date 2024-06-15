0ad9da132465659297a392f6b3518eecec13c536
Peter Sharpe <pds@mit.edu>
2024-06-15 19:59:47 -0400

    Adds NACA-RM-A50K27 wind tunnel data to study, for comparison

---
+++ tutorial/06 - Aerodynamics/01 - AeroSandbox 3D Aerodynamics Tools/assets/load_naca_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from pathlib import Path
+
+file = Path(__file__).parent / "NACA-RM-A50K27.xlsx"
+
+CL = pd.read_excel(file, sheet_name="CL")
+CD = pd.read_excel(file, sheet_name="CD")
+Cm = pd.read_excel(file, sheet_name="CM")
+
+from scipy import interpolate
+CL_to_alpha= interpolate.interp1d(
+    CL["CL"],
+    CL["alpha"],
+    fill_value="extrapolate"
+)
+
+Cm['alpha'] = CL_to_alpha(Cm['CL'])
